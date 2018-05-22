850271af959e7d7e952fcefb98e9ed982cd657eb
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-22 11:14:43 +0700

    Logical operator table

---
+++ java-beginner-guide/chapter02/LogicalOpTable.java
@@ -0,0 +1,8 @@
+class LogicalOpTable {
+  public static void main(String args[]) {
+    boolean p, q;
+    p = true; q = false;    
+    System.out.println(p + "\t" + q + "\t"); 
+    System.out.println((p & q) + "\t" + (p|q) + "\t");
+  }
+}
