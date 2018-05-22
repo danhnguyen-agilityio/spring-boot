448c70f82d7fc1d00aedb7098eb80610ae896528
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-22 14:29:19 +0700

    keyboard input

---
+++ java-beginner-guide/chapter03/KbIn.java
@@ -0,0 +1,8 @@
+class KbIn {
+  public static void main(String args[]) throws java.io.IOException {
+    char ch;
+    System.out.println("Press a key: ");
+    ch = (char) System.in.read(); // read a character from the keyboard
+    System.out.println("Your key is: " + ch);
+  } 
+}
