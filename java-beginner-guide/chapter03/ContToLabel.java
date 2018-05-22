50fd64052e543767e7236a2bcea4f125411ed106
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-22 20:59:30 +0700

    Continue label

---
+++ java-beginner-guide/chapter03/ContToLabel.java
@@ -0,0 +1,13 @@
+// page 157
+class ContToLabel {
+  public static void main(String args[]) {
+    outerloop: 
+      for(int i = 1; i < 10; i++) {
+        System.out.println("\nOuter loops pass " + i + ", Inner loop");
+        for (int j = 1; j < 10; j++) {
+          if (j == 5) continue outerloop;
+          System.out.println(j);
+        }
+      }
+  }
+}
