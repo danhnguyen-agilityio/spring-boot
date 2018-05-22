ea9a225bd3c99b8483f88f9dc5d174934cefabc5
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-22 17:14:28 +0700

    Breal label

---
+++ java-beginner-guide/chapter03/Break4.java
@@ -0,0 +1,24 @@
+class Break4 {
+  public static void main(String args[]) {
+    int i;
+
+    for (i=1; i<4; i++) {
+      one: {
+        two: {
+          three: {
+            System.out.println("\ni is " + i);
+            if (i==1) break one;
+            if (i==2) break two;
+            if (i==1) break three;
+
+            System.out.println("wont't print");
+          }
+          System.out.println("After block three");
+        }
+        System.out.println("After block two");
+      }
+      System.out.println("After blocl one");
+    }
+    System.out.println("After for.");
+  }
+}
