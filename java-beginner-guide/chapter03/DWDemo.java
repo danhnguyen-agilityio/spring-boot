a010234cfb46cec35e494cd5f7a434638a286a1e
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-22 16:32:33 +0700

    Demo while loop

---
+++ java-beginner-guide/chapter03/DWDemo.java
@@ -0,0 +1,10 @@
+class DWDemo {
+  public static void main(String args[]) throws java.io.IOException {
+    char ch;
+
+    do {
+      System.out.println("Press a key: ");
+      ch = (char) System.in.read();
+    } while(ch != 'q');
+  } 
+}
