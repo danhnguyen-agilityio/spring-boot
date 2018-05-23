06b1fc56383ca72b7044de49645aa499b4268a50
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-23 17:28:41 +0700

    Length array member

---
+++ java-beginner-guide/chapter05/LengthDemo.java
@@ -0,0 +1,16 @@
+// Use the length array member
+class LengthDemo {
+  public static void main(String args[]) {
+    int table[][] = {
+      {1, 2, 3},
+      {4, 5},
+      {6, 7, 8, 9}
+    };
+
+    System.out.println(talbe.length);
+
+    System.out.println(talbe[0].length);
+    System.out.println(talbe[1].length);
+    System.out.println(talbe[2].length);
+  }
+}
