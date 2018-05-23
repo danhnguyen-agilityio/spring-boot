0e4590370810b39df76d68d4ca947000c4f8997d
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-23 17:25:50 +0700

    Prevent division by zero using the ?

---
+++ java-beginner-guide/chapter05/NoZeroDiv.java
@@ -0,0 +1,9 @@
+class NoZeroDiv {
+  public static void main(String args[]) {
+    for (int i = -5; i < 6; i++) {
+      if (i != 0 ? true : false) {
+        System.out.println(100/i);
+      }
+    }  
+  }
+}
