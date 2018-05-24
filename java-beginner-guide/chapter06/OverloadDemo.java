ad19336ee6988e9e3b4d39c0e8eda543bf6cbb11
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-24 21:17:54 +0700

    Overloading demo

---
+++ java-beginner-guide/chapter06/OverloadDemo.java
@@ -0,0 +1,41 @@
+/** Demonstrate method overloading */
+class OverLoad {
+  void ovlDemo() {
+    System.out.println("no param");
+  }
+
+  /** Overload ovlDemo for one params */
+  void ovlDemo(int a) {
+    System.out.println("one param");
+  }
+
+  /** Overload ovlDemo for two int params */
+  int ovlDemo(int a, int b) {
+    System.out.println("two param");
+    return a + b;
+  }
+
+  /** Overload ovlDemo for differ return type */
+  // void ovlDemo(int a, int b) {
+  //   System.out.println("two int param return boolean");
+  // }
+
+  /** Overload ovlDemo for two one params */
+  double ovlDemo(double a, double b) {
+    System.out.println("two double param");
+    return a + b;
+  }
+}
+
+class OverloadDemo {
+  public static void main(String args[]) {
+    OverLoad ob = new OverLoad();
+    int resI;
+    double resD;
+
+    ob.ovlDemo();
+    ob.ovlDemo(1);
+    ob.ovlDemo(1,2);
+    ob.ovlDemo(1.3, 2.4);
+  }
+}
