3e8b588e0eb6c0e2439b85a2a32b133a0b87cb48
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-25 07:00:39 +0700

    various forms of constructor

--- java-beginner-guide/chapter07/Shape.java
+++ java-beginner-guide/chapter07/Shape.java
@@ -4,10 +4,22 @@ class TwoDShape {
   private double width;
   private double height;
 
+  /** A default constructor */
+  TwoDShape() {
+    width = 0;
+    height = 0;
+  }
+
   /** Constructor params */
   TwoDShape(double w, double h) {
     width = w;
     height = h;
+  }
+
+  /** Constructor object with equal width and height */
+  TwoDShape(double x) {
+    width = x;
+    height = x;
   }
 
   /** Accessor methods for width and height */
@@ -34,9 +46,21 @@ class TwoDShape {
 class Triangle extends TwoDShape {
   private String style;
 
+  /** A default constructor */
+  Triangle() {
+    super();
+    style = "none";
+  }
+
   Triangle(String s, double w, double h) {
     super(w, h);
     style = s;
+  }
+
+  /** One arg constructor */
+  Triangle(double x) {
+    super(x);
+    style = "filled";
   }
 
   /** Get area */
@@ -51,12 +75,25 @@ class Triangle extends TwoDShape {
 
 class Shape {
   public static void main(String args[]) {
-    Triangle t1 = new Triangle("filled", 4.0, 4.0);
-    // Triangle t2 = new Triangle();
+    Triangle t1 = new Triangle();
+    Triangle t2 = new Triangle("outlined", 8.0, 12.0);
+    Triangle t3 = new Triangle(4.0);
+
+    t1 = t2;
 
     System.out.println("Info for t1: ");
     t1.showStyle();
     t1.showDim();
     System.out.println("Area is " + t1.area());
+
+    System.out.println("Info for t2: ");
+    t2.showStyle();
+    t2.showDim();
+    System.out.println("Area is " + t2.area());
+
+    System.out.println("Info for t3: ");
+    t3.showStyle();
+    t3.showDim();
+    System.out.println("Area is " + t3.area());
   }
 }
