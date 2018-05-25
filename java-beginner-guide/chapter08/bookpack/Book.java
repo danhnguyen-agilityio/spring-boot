e364df017cb3ff0a9063e66eef7b03e695294eb9
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-25 10:24:26 +0700

    Use class from other package

--- java-beginner-guide/chapter08/bookpack/Book.java
+++ java-beginner-guide/chapter08/bookpack/Book.java
@@ -1,26 +1,17 @@
 package bookpack; // A short package demonstration
 
 /** Class Book is part of bookpack */
-class Book {
+public class Book {
   private String title;
   private String author;
 
-  Book(String t, String a) {
+  public Book(String t, String a) {
     title = t;
     author = a;
   }
 
-  void show() {
+  public void show() {
     System.out.println(title);
     System.out.println(author);
-  }
-}
-
-/** Class BookDemo is part of bookpack */
-class BookDemo {
-  public static void main(String args[]) {
-    Book book = new Book("Danh", "Tu");
-
-    book.show();
   }
 }
