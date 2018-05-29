ad6a385135f4202fa635345ce25561c73072b74f
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-29 16:06:30 +0700

    Use ordinal and compareTo method

--- java-beginner-guide/chapter12/EnumDemo.java
+++ java-beginner-guide/chapter12/EnumDemo.java
@@ -29,5 +29,43 @@ class EnumDemo {
         System.out.println("A TRAIN");
         break;
     }
+
+    System.out.println("Here are all Transport constants");
+
+    // Obtain an array of Transport constants
+    Transport allTransports[] = Transport.values();
+    for (Transport t : allTransports) {
+      System.out.println(t);
+    }
+
+    System.out.println();
+
+    tp = Transport.valueOf("AIRPLANE");
+    System.out.println("Contains " + tp);
+    System.out.println("------------------------");
+    
+    System.out.println("Ordinal values");
+    Transport tp1, tp2, tp3;
+    System.out.println("Their ordinal values");
+    for (Transport t : Transport.values()) {
+      System.out.println(t.ordinal());
+    }
+
+    tp1 = Transport.AIRPLANE;
+    tp2 = Transport.TRAIN;
+    tp3 = Transport.AIRPLANE;
+    System.out.println();
+
+    if (tp1.compareTo(tp2) < 0) {
+      System.out.println(tp + " comes before " + tp2);
+    }
+
+    if (tp1.compareTo(tp2) > 0) {
+      System.out.println(tp + " comes after " + tp2);
+    }
+
+    if (tp1.compareTo(tp3) == 0) {
+      System.out.println(tp + " equals " + tp3);
+    }
   }
 }
