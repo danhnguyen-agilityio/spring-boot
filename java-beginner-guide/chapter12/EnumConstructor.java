9a273c448a81f189a35a936dc3074bffdb6fb041
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-29 16:40:57 +0700

    Use constructor, var, method in enum

---
+++ java-beginner-guide/chapter12/EnumConstructor.java
@@ -0,0 +1,27 @@
+/** Use an enum constructor, instance variable and method */
+enum Transport {
+  CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(700), BOAT(22);
+
+  private int speed;
+
+  /** Constructor */
+  Transport(int s ) {
+    speed = s;
+  }
+
+  /** Get speed of transport */
+  int getSpeed() {
+    return speed;
+  }
+}
+
+class EnumConstructor {
+  public static void main(String args[]) {
+    Transport tp;
+
+    System.out.println("All transport speeds: ");
+    for (Transport t : Transport.values()) {
+      System.out.println(t + " typical speed is " + t.getSpeed());
+    }
+  }
+}
