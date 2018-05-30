f6dab3e551feebee3c80e7f1f80bb650e646dea9
Danh Nguyen <danhnguyentk@gmail.com>
2018-05-30 09:49:48 +0700

    Import static demo

---
+++ java-beginner-guide/chapter12/ImportStatic.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+import java.lang.Math;
+
+/** Import static */
+class ImportStatic {
+  public static void main(String args[]) {
+    System.out.println(pow(5,2));
+    System.out.println(sqrt(16));
+  }
+}
