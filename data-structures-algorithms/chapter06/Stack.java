0c6a0332cf7d67d2c5b1bb8e76b6a3505ba4ceef
Danh Nguyen <danhnguyentk@gmail.com>
2018-06-05 14:54:31 +0700

    Stack interface

---
+++ data-structures-algorithms/chapter06/Stack.java
@@ -0,0 +1,40 @@
+/** 
+ * A collection of objects that are inserted and removed according to the last in 
+ * first out principle
+ *
+ * @autho Danh Nguyen
+ */
+public interface Stack<E> {
+
+  /** 
+   * Returns the number of elements in the stack
+   * @return number of elements in the stack
+   */
+  int size();
+  
+  /** 
+   * Tests wheter the stack is empty
+   * @return true if the stack is empty, false otherwise
+   */
+  boolean isEmpty();
+
+  /**
+   * Inserts an element at the top of the stack
+   * @param e the element to be inserted
+   */
+  void push(E e);
+
+  /**
+   * Returns an element at the top of the stack
+   * @return top element in the stack (or null if empty)
+   */
+  E top();
+
+  /**
+   * Removes and returns the top element from the stack
+   * @return element removed (or null if empty)
+   */
+  E pop() {
+
+  }
+}
