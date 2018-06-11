29d81751ab7cedaf57e5fa99fa5cce0e2742bd31
Danh Nguyen <danhnguyentk@gmail.com>
2018-06-11 15:20:35 +0700

    Sorted priority queue

--- data-structures-algorithms/chapter09/SortedPriorityQueue.java
+++ data-structures-algorithms/chapter09/SortedPriorityQueue.java
@@ -1,48 +1,45 @@
-/** An implementation of a priority queue with an unsort list */
-public class UnsortedPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
+/** An implementation of a priority queue with an sorted list */
+public class SortedPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
 
   /** primary collection of priority queue entries */
   private PositionalList<Entry<K, V>> list = new LinkedPositionalList<>();
 
   /** Creates an empty priority queue based on the natural ordering of its keys */
-  public UnsortedPriorityQueue() {
+  public SortedPriorityQueue() {
     super();
   }
 
-  /** Creates an empty prioity queue using the given comparator to order keys */
+  /** Creates an empty priority queue using the given comparator to order keys */
   public UnsortedPriorityQueue(Comparator<K> comp) {
     super(comp);
-  }
-
-  /** Returns the Position of an entry having minimal key */
-  private Position<Entry<K, V>> findMin() {
-    Position<Entry<K,V>> small = list.first();
-    for (Position<Entry<K,V>> walk : list.positions()) {
-      if (compare(walk.getElement(), small.getElement()) < 0) {
-        small = walk; // found an even smaller key
-      }
-    }
-    return small;
   }
 
   /** Inserts a key-value pair and returns the entry created */
   public Entry<K,V> insert(K key, V value) throws IllegalArgumentException {
     checkKey(key);
     Entry<K,V> newest = new PQEntry<>(key, value);
-    list.addLast(newest);
+    Position<Entry<K,V>> walk = list.last();
+    if (walk != null && compare(newest, walk.getElement()) < 0) {
+      walk = list.before(walk);
+    }
+    if (walk == null) {
+      list.addFirst(newest); // new key is smallest
+    } else {
+      list.addAfter(walk, newest); // newest goes after walk
+    }
     return newest;
   }
 
   /** Returns an entry with minimal key */
   public Entry<K,V> min() {
     if (list.isEmpty()) return null;
-    return findMin().getElement();
+    return list.first().getElement();
   }
 
   /** Removes and returns an entry with minimmal ky */
   public Entry<K,V> removeMin() {
     if (list.isEmpty()) return null;
-    return list.remove(findMin());
+    return list.remove(list.first());
   }
 
   /** Returns the number of items in the priority queue */
