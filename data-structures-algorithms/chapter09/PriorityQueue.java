4cb4b18deeedc4b0e326c61da21a379855d4c08d
Danh Nguyen <danhnguyentk@gmail.com>
2018-06-12 09:30:54 +0700

    chapter09/PriorityQueue.java

--- data-structures-algorithms/chapter09/PriorityQueue.java
+++ data-structures-algorithms/chapter09/PriorityQueue.java
@@ -13,5 +13,18 @@ public interface PriorityQueue<K, V> {
   Entry<K, V> min();
 
   /** Remvoves and returns the entry having min key */
-  Entry<K, V> remove();
+  Entry<K, V> removeMin();
+
+  /** Sort priority queue */
+  public static <E> void pqSort(PositionalList<E> S, PriorityQueue<E,?> P) {
+    int  n = S.size();
+    for (int j = 0; j < n; j++) {
+      E element = S.remove(S.first());
+      P.insert(element, null); // element is key, null value
+    }
+    for (int j = 0; j < n; j++) {
+      E element = P.removeMin().getKey();
+      S.addLast(element); // the smallest key in p is next placed in S
+    }
+  }
 }
