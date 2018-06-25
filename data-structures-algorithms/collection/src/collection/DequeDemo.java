44645dbd59becb45054042c8dd539962ad371af8
Danh Nguyen <danhnguyentk@gmail.com>
2018-06-25 09:12:49 +0700

    Deque demo

---
+++ data-structures-algorithms/collection/src/collection/DequeDemo.java
@@ -0,0 +1,33 @@
+package collection;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DequeDemo {
+    public static void main(String[] args) {
+        // Stack demo
+        Deque<String> stack = new LinkedList<>();
+        stack.push("B");
+        stack.push("D");
+        stack.push("C");
+        stack.push("A");
+        stack.push("E");
+
+        for (String s : stack) {
+            System.out.println(s);
+        }
+
+        System.out.println("-------------------");
+        // Queue demo
+        Deque<String> queue = new LinkedList<>();
+        queue.offer("B");
+        queue.offer("D");
+        queue.offer("C");
+        queue.offer("A");
+        queue.offer("E");
+
+        for (String s : queue) {
+            System.out.println(s);
+        }
+    }
+}
