809b8b4d569ed68d681c1bdfce44e2663d3cc3cd
Danh Nguyen <danhnguyentk@gmail.com>
2018-06-25 17:31:41 +0700

    EnumMap demo

---
+++ data-structures-algorithms/collection/src/collection/EnumMapDemo.java
@@ -0,0 +1,23 @@
+package collection;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class EnumMapDemo {
+    public enum Days {
+        Monday, Tuesday, Wednesday, Thursday
+    }
+
+    public static void main(String[] args) {
+        EnumMap<Days, String> map = new EnumMap<>(Days.class);
+        map.put(Days.Monday, "1");
+        map.put(Days.Tuesday, "2");
+        map.put(Days.Wednesday, "3");
+        map.put(Days.Thursday, "4");
+
+        for (Map.Entry m : map.entrySet()) {
+            System.out.println(m.getKey() + "  " + m.getValue());
+        }
+
+    }
+}
