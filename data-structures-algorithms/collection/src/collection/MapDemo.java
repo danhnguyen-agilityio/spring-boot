69b874ec532c1a13ce67351748d94d1fd434af9e
Danh Nguyen <danhnguyentk@gmail.com>
2018-06-25 13:52:53 +0700

    Map demo

---
+++ data-structures-algorithms/collection/src/collection/MapDemo.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(200, "BBB");
+        map.put(300, "CCC");
+        map.put(100, "AA");
+
+        for (Map.Entry m : map.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+    }
+}
