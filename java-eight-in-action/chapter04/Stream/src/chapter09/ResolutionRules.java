3ca9c1904e0a823645755f2958946bdd9378b716
Danh Nguyen <danhnguyentk@gmail.com>
2018-07-11 14:06:31 +0700

    Conflict inherit

--- java-eight-in-action/chapter04/Stream/src/chapter09/ResolutionRules.java
+++ java-eight-in-action/chapter04/Stream/src/chapter09/ResolutionRules.java
@@ -18,6 +18,38 @@ class D implements A {
   }
 }
 
+interface E {
+  default void hello() {
+    System.out.println("A");
+  }
+}
+
+class F implements A, E {
+
+  @Override
+  public void hello() {
+    A.super.hello();
+  }
+}
+
+interface M {
+  default String getNumber() {
+    return "String";
+  }
+}
+
+interface N {
+  default Integer getNumber() {
+    return 42;
+  }
+}
+
+//class K implements M, N {
+//  public static void main(String[] args) {
+//    System.out.println(new K().getNumber());
+//  }
+//}
+
 public class ResolutionRules extends D implements B, A {
   public static void main(String[] args) {
     new ResolutionRules().hello();
