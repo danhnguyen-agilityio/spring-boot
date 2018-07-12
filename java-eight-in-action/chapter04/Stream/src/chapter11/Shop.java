166206859655aa407726c125ede062f83fb61e0b
Danh Nguyen <danhnguyentk@gmail.com>
2018-07-12 18:11:05 +0700

    Compose two async operation

--- java-eight-in-action/chapter04/Stream/src/chapter11/Shop.java
+++ java-eight-in-action/chapter04/Stream/src/chapter11/Shop.java
@@ -3,12 +3,10 @@ package chapter11;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Shop {
 
@@ -29,7 +27,14 @@ public class Shop {
   }
 
   public double getPrice(String product) {
+
     return calculatePrice(product);
+  }
+
+  public String getPriceOther(String product) {
+    double price = calculatePrice(product);
+    Discount.Code code = Discount.Code.values()[new Random().nextInt(Discount.Code.values().length)];
+    return String.format("%s:%.2f:%s", name, price,code);
   }
 
   public Future<Double> getPriceAsync1(String product) {
@@ -85,18 +90,64 @@ public class Shop {
         .collect(Collectors.toList());
   }
 
-  public List<String> findPricesAsync(String product) {
+  public List<String> findPricesOther(String product) {
     List<Shop> shops = Arrays.asList(new Shop("BestPrice"),
         new Shop("Lets Save Big"),
         new Shop("My Favorite Shop"),
         new Shop("Flower Shop"),
         new Shop("Buy It All"));
 
+    List<CompletableFuture<String>> priceFutures =
+    shops.stream()
+        .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPriceOther(product)))
+        .map(future -> future.thenApply(Quote::parse))
+        // Compose the resulting Future with another async task, applying the discount code
+        .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(
+            () -> Discount.applyDiscount(quote)
+        )))
+        .collect(Collectors.toList());
+
+    return priceFutures.stream()
+        // Wait for all the Futures in the stream to be completed and extract their respective results
+        .map(CompletableFuture::join)
+        .collect(Collectors.toList());
+
+  }
+
+  public List<String> findPricesAsync(String product) {
+    List<Shop> shops = Arrays.asList(new Shop("BestPrice"),
+        new Shop("Lets Save Big"),
+        new Shop("My Favorite Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Flower Shop"),
+        new Shop("Buy It All"));
+
+    // Create a thread pool with a number of threads equal to the minimum between 100 and the number of shops
+    Executor executor = Executors.newFixedThreadPool(Math.min(shops.size(), 100), new ThreadFactory() {
+      @Override
+      public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        // Use daemon threads - prevent the termination of the program
+        t.setDaemon(true);
+        return t;
+      }
+    });
+
     List<CompletableFuture<String>> priceFutures = shops.stream()
         // Calculate each price async with a CompletableFuture
         .map(shop -> CompletableFuture.supplyAsync(
             () -> String.format("%s price is %.2f",
-                shop.getName(), shop.getPrice(product))))
+                shop.getName(), shop.getPrice(product)), executor))
          .collect(Collectors.toList());
 
     return priceFutures.stream()
@@ -129,7 +180,8 @@ public class Shop {
   public void performanceFindPrices() {
     long start = System.nanoTime();
 //    System.out.println(findPrices("my phone"));
-    System.out.println(findPricesAsync("my phone"));
+//    System.out.println(findPricesAsync("my phone"));
+    System.out.println(findPricesOther("my phone"));
     long duration = (System.nanoTime() - start) / 1_000_000;
     System.out.println("Done in " + duration + " msecs");
   }
