4db22f4e237b7edb62c7da3a97e926542b2c7cde
Danh Nguyen <danhnguyentk@gmail.com>
2018-07-18 09:10:12 +0700

    Read user input in console

--- TweetProject/src/main/tweetapp/TweetApp.java
+++ TweetProject/src/main/tweetapp/TweetApp.java
@@ -11,7 +11,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * TweetApp implement all statistic
@@ -44,18 +46,75 @@ public class TweetApp {
     return users.stream().filter(UserService::isFemaleUser).count();
   }
 
+
+
   public static void main(String[] args) throws IOException {
     TweetApp tweetApp = new TweetApp();
-    tweetApp.users = UserService.getUsers();
-    tweetApp.posts = PostService.getPosts();
+    try {
+      tweetApp.users = UserService.getUsers();
+      tweetApp.posts = PostService.getPosts();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
-    long totalUsers = tweetApp.countAllUser();
-    System.out.println("Count all user: " + totalUsers);
+    Scanner scanner;
+    int question;
+    char confirm;
+    do {
+      System.out.println("1. Count all users");
+      System.out.println("2. Count all female users");
+      System.out.println("3. Count all male users");
+      System.out.println("4. Find all users who has been created");
+      System.out.println("  a. Within today");
+      System.out.println("  b. Within a week from today");
+      System.out.println("  c. Within a month from today");
+      System.out.println("5. Find all users who have his/her birthday within the current month");
+      System.out.println("6. Find all users who has his first name is James");
+      System.out.println("7. Find all users who has his avatar");
+      System.out.println("8. Find all users who is under 16 years old");
+      System.out.println("9. Find all users who is greater than 30 years old");
+      System.out.println("10. Find top 100 female users");
+      System.out.println("  a. Order by first name");
+      System.out.println("  b. Order by last name");
+      System.out.println("  c. Having posts within a week from today, order by post created date.");
+      System.out.println("11. Find all posts which have been created");
+      System.out.println("  a. Within today");
+      System.out.println("  b. Within a week from today");
+      System.out.println("  c. Within a month from today");
+      System.out.println("12. Find all posts of a specific user");
+      System.out.print("Please choose question that you want to see (1 -> 12): ");
 
-    long totalPost = tweetApp.countAllPost();
-    System.out.println("Count all post: " + totalPost);
+      scanner = new Scanner(System.in);
+      try {
+        question = scanner.nextInt();
+      } catch (InputMismatchException e) {
+        question = 1000; //  Default value when user enter wrong format number
+      }
 
-    long totalFemaleUsers = tweetApp.countFemaleUser();
-    System.out.println("Count all female user: " + totalFemaleUsers);
+      // Exit when user enter 0
+      if (question == 0) break;
+
+      // Choose again question if user enter value not belong from 0 -> 12
+      if (question < 0 || question > 12) {
+        System.out.println("Number question only belong from 0 -> 12. Please choose again.");
+      } else { // Show info by correspond question
+        switch (question) {
+          case 1: { // count all user
+            long totalUsers = tweetApp.countAllUser();
+            System.out.println("Count all user: " + totalUsers);
+            break;
+          }
+
+          case 2: { // count all female user
+            long totalFemaleUsers = tweetApp.countFemaleUser();
+            System.out.println("Count all female user: " + totalFemaleUsers);
+            break;
+          }
+        }
+      }
+
+      System.out.print("Would you like to continue demo app? (Y/N): ");
+      confirm = (char) System.in.read();
+    } while (confirm == 'y' || confirm == 'Y');
   }
 }
