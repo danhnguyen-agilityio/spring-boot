4b9021a50fd320df2b2b4e392293736f83a21db3
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-15 13:51:01 +0700

    Annotation constructor injection

--- ProSpring/src/main/java/com/agility/prospring4/chapter03/annotation/HelloWorldMessageProvider.java
+++ ProSpring/src/main/java/com/agility/prospring4/chapter03/annotation/HelloWorldMessageProvider.java
@@ -1,11 +1,21 @@
 package com.agility.prospring4.chapter03.annotation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("messageProvider")
 public class HelloWorldMessageProvider implements MessageProvider {
+  private String message;
 
+  @Autowired
+//  public HelloWorldMessageProvider(@Value("Annotation configuration") String message) {
+  public HelloWorldMessageProvider(String message) {
+    this.message = message;
+  }
+
+  @Autowired
   public String getMessage() {
-    return "Hello World";
+    return message;
   }
 }
