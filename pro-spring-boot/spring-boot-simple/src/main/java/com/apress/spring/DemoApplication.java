1c1ded5cadad922904fb114518b557dde57b94e5
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-20 09:43:53 +0700

    Arguments

--- pro-spring-boot/spring-boot-simple/src/main/java/com/apress/spring/DemoApplication.java
+++ pro-spring-boot/spring-boot-simple/src/main/java/com/apress/spring/DemoApplication.java
@@ -1,19 +1,27 @@
 package com.apress.spring;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import java.io.PrintStream;
+import java.util.List;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-		useSpringApplicationBuilder(args);
+		if (args != null) {
+			System.out.println(args[0]);
+		}
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	private static void customBanner(String[] args) {
@@ -34,5 +42,24 @@ public class DemoApplication {
 				.logStartupInfo(false)
 				.profiles("prod", "cloud")
 				.run(args);
+	}
+}
+
+class MyComponent {
+	private static final Logger log = (Logger) LoggerFactory.getLogger(MyComponent.class);
+
+	@Autowired
+	public MyComponent(ApplicationArguments args) {
+		boolean enable = args.containsOption("enable");
+		if (enable) {
+			log.info("## > You are enable!");
+		}
+
+		List<String> _args = args.getNonOptionArgs();
+		log.info("## > extra args");
+
+		if (!_args.isEmpty()) {
+			_args.forEach(file -> log.info(file));
+		}
 	}
 }
