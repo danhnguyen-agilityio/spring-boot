22df763e27ffc5e0df6d839d9e4241598e304fac
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-20 23:29:41 +0700

    Structure spring sql app

---
+++ pro-spring-boot/sql-app/src/main/java/com/agility/spring/SqlAppApplication.java
@@ -0,0 +1,12 @@
+package com.agility.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAppApplication.class, args);
+	}
+}
