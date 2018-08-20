95190da32c955824eff6ffe63fb34db5c5a0494c
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-20 09:00:05 +0700

    Custom banner

--- pro-spring-boot/spring-boot-simple/src/main/java/com/apress/spring/DemoApplication.java
+++ pro-spring-boot/spring-boot-simple/src/main/java/com/apress/spring/DemoApplication.java
@@ -1,12 +1,26 @@
 package com.apress.spring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//		SpringApplication.run(DemoApplication.class, args);
+
+		// Custom banner
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setBanner(new Banner() {
+			@Override
+			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+				out.print("\n\n\tThis is my own banner!\n\n".toUpperCase());
+			}
+		});
+		app.run(args);
 	}
 }
