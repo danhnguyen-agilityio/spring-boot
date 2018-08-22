2d23fad213a258f530764ab3e45699b8da968e9a
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-22 15:52:50 +0700

    Create structure example one to many relationship

---
+++ pro-spring-boot/spring-data/jpa-one2many/src/main/java/com/agility/jpa/JpaOne2manyApplication.java
@@ -0,0 +1,12 @@
+package com.agility.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOne2manyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOne2manyApplication.class, args);
+	}
+}
