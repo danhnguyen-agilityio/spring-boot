e8e5dd48f705d62cbbed1db7690b2bda4258a747
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-25 01:07:35 +0700

    Structure example multi env

---
+++ pro-spring-boot/multi-environment/src/main/java/com/example/multienvironment/MultiEnvironmentApplication.java
@@ -0,0 +1,12 @@
+package com.example.multienvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiEnvironmentApplication.class, args);
+	}
+}
