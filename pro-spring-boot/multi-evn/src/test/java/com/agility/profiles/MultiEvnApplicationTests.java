0413b7cc7f24fa7620d37b27215f61476b4f2c53
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-25 16:21:34 +0700

    Structure example use Multi Env with @Profile annotation

--- pro-spring-boot/multi-evn/src/test/java/com/agility/profiles/MultiEvnApplicationTests.java
+++ pro-spring-boot/multi-evn/src/test/java/com/agility/profiles/MultiEvnApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.multienvironment;
+package com.agility.profiles;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MultiEnvironmentApplicationTests {
+public class MultiEvnApplicationTests {
 
 	@Test
 	public void contextLoads() {
