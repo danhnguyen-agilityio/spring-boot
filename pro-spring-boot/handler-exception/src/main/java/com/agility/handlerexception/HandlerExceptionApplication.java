229e0085cb1818ece21b94d71bfb83d6febb929e
Danh Nguyen <danhnguyentk@gmail.com>
2018-08-25 20:09:18 +0700

    Create handler exception example

---
+++ pro-spring-boot/handler-exception/src/main/java/com/agility/handlerexception/HandlerExceptionApplication.java
@@ -0,0 +1,12 @@
+package com.agility.handlerexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlerExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlerExceptionApplication.class, args);
+	}
+}
---
+++ pro-spring-boot/handler-exception/src/test/java/com/agility/handlerexception/HandlerExceptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agility.handlerexception;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HandlerExceptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
