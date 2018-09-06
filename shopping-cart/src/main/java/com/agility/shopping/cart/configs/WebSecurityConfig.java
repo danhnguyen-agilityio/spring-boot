7f2a4b6280c2b97624f1fc359dd2e2382d76e43d
Danh Nguyen <danhnguyentk@gmail.com>
2018-09-07 01:10:39 +0700

    Authorize user for product APIs

--- shopping-cart/src/main/java/com/agility/shopping/cart/configs/WebSecurityConfig.java
+++ shopping-cart/src/main/java/com/agility/shopping/cart/configs/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.agility.shopping.cart.configs;
 
+import com.agility.shopping.cart.constants.RoleType;
 import com.agility.shopping.cart.filters.JWTAuthenticationFilter;
 import com.agility.shopping.cart.filters.JWTAuthorizationFilter;
 import com.agility.shopping.cart.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -48,6 +50,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+            // Authenticated user can access to api get product and list product
+            .antMatchers(HttpMethod.GET, "/products/**").authenticated()
+            // Only user admin can access to api create, update, delete product
+            .antMatchers( "/products/**").hasAuthority(RoleType.ADMIN.getName())
             .anyRequest().authenticated()
             .and()
             // The authentication filter
