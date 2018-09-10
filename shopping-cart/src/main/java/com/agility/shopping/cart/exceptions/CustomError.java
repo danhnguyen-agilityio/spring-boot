fdecd8664212c2df6e7abadf163682ad48a4a9b9
Danh Nguyen <danhnguyentk@gmail.com>
2018-09-10 18:17:39 +0700

    Define error shopping cart already done

--- shopping-cart/src/main/java/com/agility/shopping/cart/exceptions/CustomError.java
+++ shopping-cart/src/main/java/com/agility/shopping/cart/exceptions/CustomError.java
@@ -12,7 +12,8 @@ public enum CustomError {
     USER_NOT_FOUND(420, "User not found"),
     SHOPPING_CART_EXIST(430, "Shopping cart is already existed"),
     SHOPPING_CART_NOT_FOUND(431, "Shopping cart not found"),
-    SHOPPING_CART_FORBIDDEN(432, "You don't have permission to update shopping cart of other user");
+    SHOPPING_CART_FORBIDDEN(432, "You don't have permission to update shopping cart of other user"),
+    SHOPPING_CART_DONE(433, "Shopping cart is already done");
 
     private int code;
     private String message;
