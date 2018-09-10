75e97913d63c39048867da4679bf98b7772b2983
Danh Nguyen <danhnguyentk@gmail.com>
2018-09-10 18:21:03 +0700

    Unit test feature create cart item

---
+++ shopping-cart/src/test/java/com/agility/shopping/cart/controllers/CartItemControllerTest.java
@@ -0,0 +1,295 @@
+package com.agility.shopping.cart.controllers;
+
+import com.agility.shopping.cart.constants.ShoppingCartStatus;
+import com.agility.shopping.cart.dto.CartItemRequest;
+import com.agility.shopping.cart.mappers.CartItemMapper;
+import com.agility.shopping.cart.models.CartItem;
+import com.agility.shopping.cart.models.Product;
+import com.agility.shopping.cart.models.ShoppingCart;
+import com.agility.shopping.cart.models.User;
+import com.agility.shopping.cart.repositories.CartItemRepository;
+import com.agility.shopping.cart.repositories.ProductRepository;
+import com.agility.shopping.cart.repositories.ShoppingCartRepository;
+import com.agility.shopping.cart.services.TokenAuthenticationService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static com.agility.shopping.cart.configs.WebSecurityConfig.CART_ITEM_URL;
+import static com.agility.shopping.cart.constants.SecurityConstants.HEADER_STRING;
+import static com.agility.shopping.cart.exceptions.CustomError.PRODUCT_NOT_FOUND;
+import static com.agility.shopping.cart.exceptions.CustomError.SHOPPING_CART_NOT_FOUND;
+import static com.agility.shopping.cart.utils.ConvertUtil.convertObjectToJsonBytes;
+import static com.agility.shopping.cart.utils.FakerUtil.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * This class test RESTful api for cart item
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class CartItemControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private FilterChainProxy filterChainProxy;
+
+    @MockBean
+    private ShoppingCartRepository shoppingCartRepository;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @MockBean
+    private CartItemRepository cartItemRepository;
+
+    @MockBean
+    private CartItemMapper cartItemMapper;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(webApplicationContext)
+            .addFilter(filterChainProxy)
+            .build();
+    }
+
+    // =========================================================
+    // Test Create Cart Item
+    // =========================================================
+
+    /**
+     * Test create cart item throw forbidden exception for admin user
+     */
+    @Test
+    public void testCreateCartItemThrowForbiddenExceptionForAdminUser() throws Exception {
+        // Create admin token
+        String token = generateAdminToken();
+
+        // Call api
+        mockMvc.perform(post(CART_ITEM_URL)
+            .header(HEADER_STRING, token))
+            .andExpect(status().isForbidden());
+    }
+
+    /**
+     * Test create cart item throw resource not found exception
+     * when no shopping cart by given shopping cart id and user id
+     */
+    @Test
+    public void testCreateCartItemThrowResourceNotFoundExceptionWhenNoShoppingCartByGivenShoppingCartIdAndUserId()
+        throws Exception {
+
+        // Mock member user
+        User user = fakeMemberUser();
+
+        // Fake token
+        String token = TokenAuthenticationService.createToken(user);
+
+        // Mock cart item
+        CartItemRequest cartItemRequest = fakeCartItemRequest();
+
+        // Mock method
+        when(shoppingCartRepository.findOne(cartItemRequest.getShoppingCartId(), user.getId())).thenReturn(null);
+
+        // Call api
+        mockMvc.perform(post(CART_ITEM_URL)
+            .header(HEADER_STRING, token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(convertObjectToJsonBytes(cartItemRequest)))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.code", is(SHOPPING_CART_NOT_FOUND.code())));
+
+        verify(shoppingCartRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), user.getId());
+        verifyNoMoreInteractions(shoppingCartRepository);
+    }
+
+    /**
+     * Test create cart item throw resource not found exception when shopping cart done
+     */
+    @Test
+    public void testCreatedCartItemThrowBadRequestExceptionWhenShoppingCartDone() throws Exception {
+        // Mock member user
+        User user = fakeMemberUser();
+
+        // Fake token
+        String token = TokenAuthenticationService.createToken(user);
+
+        // Mock cart item request and shopping cart with status DONE
+        CartItemRequest cartItemRequest = fakeCartItemRequest();
+        ShoppingCart shoppingCart = fakeShoppingCart(ShoppingCartStatus.DONE);
+
+        // Mock method
+        when(shoppingCartRepository.findOne(cartItemRequest.getShoppingCartId(), user.getId())).thenReturn(shoppingCart);
+
+        // Call api
+        mockMvc.perform(post(CART_ITEM_URL)
+            .header(HEADER_STRING, token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(convertObjectToJsonBytes(cartItemRequest)))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.code", is(SHOPPING_CART_NOT_FOUND.code())));
+
+        verify(shoppingCartRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), user.getId());
+        verifyNoMoreInteractions(shoppingCartRepository);
+    }
+
+    /**
+     * Test create cart item throw resource not found exception when product id not exist
+     */
+    @Test
+    public void testCreateCartItemThrowResourceNotFoundExceptionWhenProductIdNotExist() throws Exception {
+        // Mock member user
+        User user = fakeMemberUser();
+
+        // Fake token
+        String token = TokenAuthenticationService.createToken(user);
+
+        // Mock data
+        CartItemRequest cartItemRequest = fakeCartItemRequest();
+        ShoppingCart shoppingCart = fakeShoppingCart(ShoppingCartStatus.INPROGRESS);
+
+        // Mock method
+        when(shoppingCartRepository.findOne(cartItemRequest.getShoppingCartId(), user.getId())).thenReturn(shoppingCart);
+        when(productRepository.findOne(cartItemRequest.getProductId())).thenReturn(null);
+
+        // Call api
+        mockMvc.perform(post(CART_ITEM_URL)
+            .header(HEADER_STRING, token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(convertObjectToJsonBytes(cartItemRequest)))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.code", is(PRODUCT_NOT_FOUND.code())));
+
+        verify(shoppingCartRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), user.getId());
+        verify(productRepository, times(1)).
+            findOne(cartItemRequest.getProductId());
+        verifyNoMoreInteractions(shoppingCartRepository);
+        verifyNoMoreInteractions(productRepository);
+    }
+
+    /**
+     * Test create cart item success when cart item with given shopping cart id and product id not exists
+     */
+    @Test
+    public void testCreateCartItemSuccessWhenCartItemNotExist() throws Exception {
+        // Mock member user
+        User user = fakeMemberUser();
+
+        // Generate token
+        String token = TokenAuthenticationService.createToken(user);
+
+        // Mock cart item request, shopping cart, product
+        CartItemRequest cartItemRequest = fakeCartItemRequest();
+        ShoppingCart shoppingCart = fakeShoppingCart(ShoppingCartStatus.INPROGRESS);
+        Product product = fakeProduct();
+        long quantity = generateLongNumber();
+        CartItem cartItem = cartItemMapper.toCartItem(cartItemRequest);
+        cartItem.setQuantity(quantity);
+        cartItem.setShoppingCart(shoppingCart);
+        cartItem.setProduct(product);
+
+        // Mock method
+        when(shoppingCartRepository.findOne(cartItemRequest.getShoppingCartId(), user.getId())).thenReturn(shoppingCart);
+        when(productRepository.findOne(cartItemRequest.getProductId())).thenReturn(product);
+        when(cartItemRepository.findOne(cartItemRequest.getShoppingCartId(), cartItemRequest.getProductId()))
+            .thenReturn(null);
+        when(cartItemRepository.save(any(CartItem.class))).thenReturn(cartItem);
+
+        // Call api
+        mockMvc.perform(post(CART_ITEM_URL)
+            .header(HEADER_STRING, token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(convertObjectToJsonBytes(cartItemRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.quantity", is(quantity)))
+            .andExpect(jsonPath("$.shoppingCart.id", is(cartItem.getShoppingCart().getId())))
+            .andExpect(jsonPath("$.product.id", is(cartItem.getProduct().getId())));
+
+        verify(shoppingCartRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), user.getId());
+        verify(productRepository, times(1)).
+            findOne(cartItemRequest.getProductId());
+        verify(cartItemRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), cartItemRequest.getProductId());
+        verify(cartItemRepository, times(1)).
+            save(any(CartItem.class));
+        verifyNoMoreInteractions(shoppingCartRepository);
+        verifyNoMoreInteractions(productRepository);
+        verifyNoMoreInteractions(cartItemRepository);
+    }
+
+    /**
+     * Test create cart item success when cart item with given shopping cart id and product id already exists
+     */
+    // FIXME:: Refactor test case
+    @Test
+    public void testCreateCartItemSuccessWhenCartItemExist() throws Exception {
+        // Mock member user
+        User user = fakeMemberUser();
+
+        // Generate token
+        String token = TokenAuthenticationService.createToken(user);
+
+        // Mock cart item request, shopping cart, product
+        CartItemRequest cartItemRequest = fakeCartItemRequest();
+        ShoppingCart shoppingCart = fakeShoppingCart(ShoppingCartStatus.INPROGRESS);
+        Product product = fakeProduct();
+        long quantity = generateLongNumber();
+        CartItem cartItem = cartItemMapper.toCartItem(cartItemRequest);
+        cartItem.setQuantity(quantity);
+        cartItem.setShoppingCart(shoppingCart);
+        cartItem.setProduct(product);
+
+        // Mock method
+        when(shoppingCartRepository.findOne(cartItemRequest.getShoppingCartId(), user.getId())).thenReturn(shoppingCart);
+        when(productRepository.findOne(cartItemRequest.getProductId())).thenReturn(product);
+        when(cartItemRepository.findOne(cartItemRequest.getShoppingCartId(), cartItemRequest.getProductId()))
+            .thenReturn(cartItem);
+        when(cartItemRepository.save(any(CartItem.class))).thenReturn(cartItem);
+
+        // Call api
+        mockMvc.perform(post(CART_ITEM_URL)
+            .header(HEADER_STRING, token)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(convertObjectToJsonBytes(cartItemRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.quantity", is(quantity + cartItemRequest.getQuantity())))
+            .andExpect(jsonPath("$.shoppingCart.id", is(cartItem.getShoppingCart().getId())))
+            .andExpect(jsonPath("$.product.id", is(cartItem.getProduct().getId())));
+
+        verify(shoppingCartRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), user.getId());
+        verify(productRepository, times(1)).
+            findOne(cartItemRequest.getProductId());
+        verify(cartItemRepository, times(1)).
+            findOne(cartItemRequest.getShoppingCartId(), cartItemRequest.getProductId());
+        verify(cartItemRepository, times(1)).
+            save(any(CartItem.class));
+        verifyNoMoreInteractions(shoppingCartRepository);
+        verifyNoMoreInteractions(productRepository);
+        verifyNoMoreInteractions(cartItemRepository);
+    }
+
+}
--- shopping-cart/src/test/java/com/agility/shopping/cart/utils/FakerUtil.java
+++ shopping-cart/src/test/java/com/agility/shopping/cart/utils/FakerUtil.java
@@ -1,7 +1,11 @@
 package com.agility.shopping.cart.utils;
 
 import com.agility.shopping.cart.constants.RoleType;
+import com.agility.shopping.cart.constants.ShoppingCartStatus;
+import com.agility.shopping.cart.dto.CartItemRequest;
 import com.agility.shopping.cart.dto.ShoppingCartRequest;
+import com.agility.shopping.cart.models.Product;
+import com.agility.shopping.cart.models.Role;
 import com.agility.shopping.cart.models.ShoppingCart;
 import com.agility.shopping.cart.models.User;
 import com.agility.shopping.cart.services.TokenAuthenticationService;
@@ -18,12 +22,40 @@ import java.util.concurrent.ThreadLocalRandom;
 public class FakerUtil {
 
     /**
-     * Fake user with arbitrary username and password
+     * Fake user
      *
      * @return User
      */
     public static User fakeUser() {
-        return new User(generateString(), generateString());
+        User user = new User();
+        user.setId(generateLongNumber());
+        user.setUsername(generateString());
+        user.setPassword(generateString());
+        return user;
+    }
+
+    /**
+     * Fake admin user
+     *
+     * @return Admin user
+     */
+    public static User fakeAdminUser() {
+        User user = fakeUser();
+        Set<Role> roles = Sets.newSet(new Role(RoleType.ADMIN.getName()));
+        user.setRoles(roles);
+        return user;
+    }
+
+    /**
+     * Fake member user
+     *
+     * @return Member user
+     */
+    public static User fakeMemberUser() {
+        User user = fakeUser();
+        Set<Role> roles = Sets.newSet(new Role(RoleType.MEMBER.getName()));
+        user.setRoles(roles);
+        return user;
     }
 
     /**
@@ -34,6 +66,31 @@ public class FakerUtil {
      */
     public static User fakeUser(String username) {
         return new User(username, generateString());
+    }
+
+    /**
+     * Fake product
+     *
+     * @return Product
+     */
+    public static Product fakeProduct() {
+        Product product = new Product();
+        product.setId(generateLongNumber());
+        product.setName(generateString());
+        product.setPrice(generateLongNumber());
+        return product;
+    }
+
+    /**
+     * Fake shopping cart data with
+     *
+     * @param shoppingCartStatus
+     * @return
+     */
+    public static ShoppingCart fakeShoppingCart(ShoppingCartStatus shoppingCartStatus) {
+        ShoppingCart shoppingCart = fakeShoppingCart();
+        shoppingCart.setStatus(shoppingCartStatus.getName());
+        return shoppingCart;
     }
 
     /**
@@ -67,6 +124,19 @@ public class FakerUtil {
      */
     public static ShoppingCartRequest fakeShoppingCartRequest() {
         return new ShoppingCartRequest(generateString(4, 30), generateString(10, 30));
+    }
+
+    /**
+     * Fake cart item request
+     *
+     * @return Cart item request
+     */
+    public static CartItemRequest fakeCartItemRequest() {
+        CartItemRequest cartItemRequest = new CartItemRequest();
+        cartItemRequest.setShoppingCartId(generateLongNumber());
+        cartItemRequest.setProductId(generateLongNumber());
+        cartItemRequest.setQuantity(generateLongNumber(1));
+        return cartItemRequest;
     }
 
     /**
@@ -107,6 +177,26 @@ public class FakerUtil {
         return TokenAuthenticationService.createToken(username, roles);
     }
 
+    /**
+     * Generate long number with given min value
+     *
+     * @param min Min value of created long number
+     * @return Long number
+     */
+    public static long generateLongNumber(long min) {
+        return generateLongNumber(min, Long.MAX_VALUE);
+    }
+
+    /**
+     * Generate long number with given min and max value
+     *
+     * @param min Min value of created long number
+     * @param max Max value of created long number
+     * @return Long number
+     */
+    public static long generateLongNumber(long min, long max) {
+        return ThreadLocalRandom.current().nextLong(Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
     /**
      * Generate long number
