7309a084f168d1aa1ee31ff583e556788fa7ff75
Danh Nguyen <danhnguyentk@gmail.com>
2018-09-16 16:36:17 +0700

    Add file MessageConstant

---
+++ shopping-cart/src/main/java/com/agility/shopping/cart/constants/MessageConstant.java
@@ -0,0 +1,8 @@
+package com.agility.shopping.cart.constants;
+
+public class MessageConstant {
+    public static final String PRODUCT_DELETE_SUCCESS = "Delete product successfully";
+    public static final String SHOPPING_CART_DELETE_SUCCESS = "Delete shopping cart successfully";
+    public static final String CART_ITEM_DELETE_SUCCESS = "Delete cart item successfully";
+    public static final String SHOPPING_CART_CHECKOUT_SUCCESS = "Checkout shopping cart successfully";
+}
