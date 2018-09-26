53cf078a06671fe5eb3409ac63a2f62bc3d71dde
Danh Nguyen <danhnguyentk@gmail.com>
2018-09-26 13:57:45 +0700

    Concept about BeanPostProcessor, InitializationBean, DisposableBean, @PostContruct, @PreDestroy

--- pro-spring-boot/bean/spring-bean-lifecycle/src/main/java/com/agility/springbeanlifecycle/SpringBeanLifecycleApplication.java
+++ pro-spring-boot/bean/spring-bean-lifecycle/src/main/java/com/agility/springbeanlifecycle/SpringBeanLifecycleApplication.java
@@ -11,6 +11,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 // https://grokonez.com/spring-framework/spring-core/spring-bean-life-cycle-callback-interface-custom-method-annotation-aware-interface
 // https://grokonez.com/spring-framework/spring-core/spring-bean-post-processors
 // https://www.mkyong.com/spring-boot/how-to-display-all-beans-loaded-by-spring-boot/
+// The BeanPostProcessor interface helps us provide our own methods before the initialization of beans and likewise after the initialization of the bean
+// InitializingBean and DisposableBean callback interfaces will be implemented to override 2 method afterPropertiesSet() and destroy()
+// @PostConstruct and @PreDestroy annotations will be annotated to methods:
+// @PostConstruct: is called after constructor have bean called
+// @PreDestroy: is called before destroy bean
 @SpringBootApplication
 public class SpringBeanLifecycleApplication implements CommandLineRunner {
 
