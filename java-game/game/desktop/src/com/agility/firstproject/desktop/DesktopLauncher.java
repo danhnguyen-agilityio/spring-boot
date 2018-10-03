6f7249c16244360aaec8b4b85800102bcf8a6cbb
DESKTOP-9VUKGMN\danhnguyen <danh.nguyen@asnet.com.vn>
2018-10-03 09:41:03 +0700

    SpriteBatch, Sprite, Texture, TextureRegion in libGDX

---
+++ java-game/game/desktop/src/com/agility/firstproject/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.agility.firstproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.agility.firstproject.firstproject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new firstproject(), config);
+
+	}
+}
