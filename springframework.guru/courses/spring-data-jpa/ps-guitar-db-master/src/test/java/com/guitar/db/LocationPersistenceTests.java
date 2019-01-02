b48b966de5ebd2f45856452a63766f134f46b9c8
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-01-02 09:27:58 +0700

    Install spring jpa

--- springframework.guru/courses/spring-data-jpa/ps-guitar-db-master/src/test/java/com/guitar/db/LocationPersistenceTests.java
+++ springframework.guru/courses/spring-data-jpa/ps-guitar-db-master/src/test/java/com/guitar/db/LocationPersistenceTests.java
@@ -1,12 +1,14 @@
 package com.guitar.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import com.guitar.db.repository.LocationJpaRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class LocationPersistenceTests {
 	@Autowired
 	private LocationRepository locationRepository;
 
+	@Autowired
+	private LocationJpaRepository locationJpaRepository;
+
 	@PersistenceContext
 	private EntityManager entityManager;
+
+	@Test
+	public void testJpaFind() {
+		List<Location> locations = locationJpaRepository.findAll();
+		assertNotNull(locations);
+	}
 
 	@Test
 	@Transactional
