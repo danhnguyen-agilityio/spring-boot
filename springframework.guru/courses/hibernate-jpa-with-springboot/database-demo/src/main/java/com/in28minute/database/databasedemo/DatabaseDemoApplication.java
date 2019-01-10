71f103e67a9ba93debdf74e5c1ff0800cfab2d6e
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-10 17:15:52 +0700

    Setting up a project with JDBC, JPA, H2 and Web Dependencies, Launching up H2 Console

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/database-demo/src/main/java/com/in28minute/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,14 @@
+package com.in28minute.database.databasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+}
+
