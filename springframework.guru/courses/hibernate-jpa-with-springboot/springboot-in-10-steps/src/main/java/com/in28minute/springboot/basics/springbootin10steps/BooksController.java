7bf7ee2d3d9a3ea037456418e40656bc8bfc3a1b
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-10 15:10:49 +0700

    Create a simple rest controller

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/springboot-in-10-steps/src/main/java/com/in28minute/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,18 @@
+package com.in28minute.springboot.basics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Arrays.asList(
+            new Book(1l, "Mastering Spring 5.0", "Ranga Karanam")
+        );
+    }
+}
