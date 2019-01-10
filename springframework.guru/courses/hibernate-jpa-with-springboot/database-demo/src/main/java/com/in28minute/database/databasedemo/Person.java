ee05edb4fffb3889d9e815c3dfd7b6f341478aeb
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-10 17:35:52 +0700

    Implement findAll persons Spring JDBC Query Method

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/database-demo/src/main/java/com/in28minute/database/databasedemo/Person.java
@@ -0,0 +1,50 @@
+package com.in28minute.database.databasedemo;
+
+import java.util.Date;
+
+public class Person {
+
+    private long id;
+    private String name;
+    private String location;
+    private Date birthDate;
+
+    public Person(long id, String name, String location, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/database-demo/src/main/java/com/in28minute/database/databasedemo/PersonJdbcDao.java
@@ -0,0 +1,19 @@
+package com.in28minute.database.databasedemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
