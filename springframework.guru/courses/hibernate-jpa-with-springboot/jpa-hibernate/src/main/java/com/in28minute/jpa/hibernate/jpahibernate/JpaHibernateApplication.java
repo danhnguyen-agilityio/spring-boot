1c8459248445c1db744ac4aea011eb277ae00efd
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-11 15:16:47 +0700

    Create jpa project with h2 and spring boot

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/JpaHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.in28minute.jpa.hibernate.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateApplication.class, args);
+	}
+
+}
+
