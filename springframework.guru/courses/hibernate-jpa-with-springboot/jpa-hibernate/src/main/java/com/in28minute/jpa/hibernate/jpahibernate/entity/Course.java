029cdd97efa38fc724877e062772e9a323aa185f
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-11 15:46:25 +0700

    Create entity course

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/entity/Course.java
@@ -0,0 +1,38 @@
+package com.in28minute.jpa.hibernate.jpahibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Course %s] ",name);
+    }
+}
