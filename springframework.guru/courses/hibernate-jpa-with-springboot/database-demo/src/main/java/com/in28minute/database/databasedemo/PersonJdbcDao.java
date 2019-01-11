46dd973bd2e3cb29081af55a4947fdb50caef715
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-11 10:17:39 +0700

    Create custom row mapper

--- springframework.guru/courses/hibernate-jpa-with-springboot/database-demo/src/main/java/com/in28minute/database/databasedemo/PersonJdbcDao.java
+++ springframework.guru/courses/hibernate-jpa-with-springboot/database-demo/src/main/java/com/in28minute/database/databasedemo/PersonJdbcDao.java
@@ -3,19 +3,34 @@ package com.in28minute.database.databasedemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public class PersonJdbcDao {
 
+    class PersonRowMapper implements RowMapper<Person> {
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+            Person person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setLocation(resultSet.getString("location"));
+            person.setBirthDate(resultSet.getTimestamp("birth_date"));
+            return person;
+        }
+    }
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     public List<Person> findAll() {
-        return jdbcTemplate.query("select * from Person", new BeanPropertyRowMapper<>(Person.class));
+        return jdbcTemplate.query("select * from Person", new PersonRowMapper());
     }
 
     public Person findById(int id) {
