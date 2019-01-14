b641628981854c5559bb4a901622e5137a1339c8
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-14 15:44:04 +0700

    Retrieve student with passport and eager fetch

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/test/java/com/in28minute/jpa/hibernate/jpahibernate/repository/StudentRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.in28minute.jpa.hibernate.jpahibernate.repository;
+
+import com.in28minute.jpa.hibernate.jpahibernate.entity.Student;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    public void retrieveStudentAndPassportDetails() {
+        Student student = entityManager.find(Student.class, 20001L);
+        logger.info("student -> {}", student);
+        logger.info("passport -> {}", student.getPassport());
+    }
+}
