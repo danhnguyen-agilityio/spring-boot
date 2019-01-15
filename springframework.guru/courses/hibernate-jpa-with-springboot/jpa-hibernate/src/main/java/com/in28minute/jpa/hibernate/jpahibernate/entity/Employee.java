77201c96ec6de530ca2a4c57d675a4d784bf03c2
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-15 16:24:11 +0700

    Single table for inheritance hierarchies

--- springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/entity/Employee.java
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/entity/Employee.java
@@ -1,11 +1,10 @@
 package com.in28minute.jpa.hibernate.jpahibernate.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "EmployeeType")
 public abstract class Employee {
 
     @Id
