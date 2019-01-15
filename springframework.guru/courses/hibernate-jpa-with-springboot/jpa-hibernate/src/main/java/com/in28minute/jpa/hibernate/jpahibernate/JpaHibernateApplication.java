fab95c87cc84f55c31005888f1921b9d77d85b22
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-15 16:11:46 +0700

    Insert employee

--- springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/JpaHibernateApplication.java
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/JpaHibernateApplication.java
@@ -1,9 +1,8 @@
 package com.in28minute.jpa.hibernate.jpahibernate;
 
-import com.in28minute.jpa.hibernate.jpahibernate.entity.Course;
-import com.in28minute.jpa.hibernate.jpahibernate.entity.Review;
-import com.in28minute.jpa.hibernate.jpahibernate.entity.Student;
+import com.in28minute.jpa.hibernate.jpahibernate.entity.*;
 import com.in28minute.jpa.hibernate.jpahibernate.repository.CourseRepository;
+import com.in28minute.jpa.hibernate.jpahibernate.repository.EmployeeRepository;
 import com.in28minute.jpa.hibernate.jpahibernate.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +25,9 @@ public class JpaHibernateApplication implements CommandLineRunner {
 
 	@Autowired
 	StudentRepository studentRepository;
+
+	@Autowired
+	EmployeeRepository employeeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaHibernateApplication.class, args);
@@ -50,9 +53,16 @@ public class JpaHibernateApplication implements CommandLineRunner {
 //		reviews.add(new Review("4", "so bad"));
 //		courseRepository.addReviewsForCourse(10003L, reviews);
 
-		studentRepository.insertStudentAndCourse(
-			new Student("Jack"),
-			new Course("Microservices in 100 steps"));
+//		studentRepository.insertStudentAndCourse(
+//			new Student("Jack"),
+//			new Course("Microservices in 100 steps"));
+
+		insertEmployee();
+	}
+
+	private void insertEmployee() {
+		employeeRepository.insert(new PartTimeEmployee("Jill", new BigDecimal(50)));
+		employeeRepository.insert(new FullTimeEmployee("Jack", new BigDecimal(10000)));
 	}
 }
 
---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/repository/EmployeeRepository.java
@@ -0,0 +1,29 @@
+package com.in28minute.jpa.hibernate.jpahibernate.repository;
+
+import com.in28minute.jpa.hibernate.jpahibernate.entity.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional
+public class EmployeeRepository {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    EntityManager em;
+
+    public void insert(Employee employee) {
+        em.persist(employee);
+    }
+
+    public List<Employee> retrieveAllEmployees() {
+        return em.createQuery("select e from Employee e", Employee.class).getResultList();
+    }
+}
