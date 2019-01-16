642c7bb3cab966065e3b03860b53d85396e7728e
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-16 15:02:02 +0700

    Test crud operation in repository

--- springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/test/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CourseSpringDataRepositoryTest.java
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/test/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CourseSpringDataRepositoryTest.java
@@ -3,6 +3,8 @@ package com.in28minute.jpa.hibernate.jpahibernate.repository;
 import com.in28minute.jpa.hibernate.jpahibernate.entity.Course;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +16,8 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CourseSpringDataRepositoryTest {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     CourseSpringDataRepository repository;
 
@@ -27,5 +31,13 @@ public class CourseSpringDataRepositoryTest {
     public void findById_CourseNotPresent() {
         Optional<Course> courseOptional = repository.findById(10004L);
         assertFalse(courseOptional.isPresent());
+    }
+
+    @Test
+    public void playingAroundWithSpringDataRepository() {
+        logger.info("Courses -> {}", repository.findAll());
+        // Courses -> [[Course David] , [Course Spring Boot in 100 Steps] , [Course Naven] ]
+        
+        logger.info("Count -> {}", repository.count());
     }
 }
