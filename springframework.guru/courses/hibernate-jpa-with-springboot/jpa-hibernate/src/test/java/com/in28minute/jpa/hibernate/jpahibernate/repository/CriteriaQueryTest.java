f3b7a8182f4a7cd7056d7173de14ec74f0846b25
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-16 09:47:42 +0700

    Retrieving all courses use criteria query

---
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/test/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CriteriaQueryTest.java
@@ -0,0 +1,51 @@
+package com.in28minute.jpa.hibernate.jpahibernate.repository;
+
+import com.in28minute.jpa.hibernate.jpahibernate.entity.Course;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CriteriaQueryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void jpql_basic() {
+        // Select c From Course c
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the expected result object
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery cq = cb.createQuery();
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+
+        // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // Typed Query -> [[Course David] , [Course Lusi] , [Course Naven] ]
+    }
+}
