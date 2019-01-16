91bf1d74c55135f60b735dc81130df63afdb5a76
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-16 10:18:33 +0700

    get courses like 100 steps with Criteria Query

--- springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/resources/data.sql
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into course (id, name, last_updated_date, created_date) values (10001, 'David', sysdate(), sysdate());
-insert into course (id, name, last_updated_date, created_date) values (10002, 'Lusi', sysdate(), sysdate());
+insert into course (id, name, last_updated_date, created_date) values (10002, 'Spring Boot in 100 Steps', sysdate(), sysdate());
 insert into course (id, name, last_updated_date, created_date) values (10003, 'Naven', sysdate(), sysdate());
 
 insert into passport(id, number) values (40001, 'E123456');
--- springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/test/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CriteriaQueryTest.java
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/test/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CriteriaQueryTest.java
@@ -13,6 +13,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class CriteriaQueryTest {
 
     @Test
     public void jpql_basic() {
-        // Select c From Course c
+        // Select c From Course c where name like '%100 Steps'
 
         // 1. Use Criteria Builder to create a Criteria Query returning the expected result object
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -47,5 +48,31 @@ public class CriteriaQueryTest {
 
         logger.info("Typed Query -> {}", resultList);
         // Typed Query -> [[Course David] , [Course Lusi] , [Course Naven] ]
+    }
+
+    @Test
+    public void all_courses_having_100Steps() {
+        // Select c From Course c where name like '%100 Steps'
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the expected result object
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery cq = cb.createQuery();
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Predicate like100Steps = cb.like(courseRoot.get("name"), "%100 Steps");
+
+        // 4. Add Predicates etc to the Criteria Query
+        cq.where(like100Steps);
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // Typed Query -> [[Course Spring Boot in 100 Steps] ]
     }
 }
