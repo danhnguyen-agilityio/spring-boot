a50b3493d9a2101e0e76f0a865475925f15f529c
Danh Nguyen <danhnguyentk@gmail.com>
2019-01-16 15:45:08 +0700

    Custom query use jpql

--- springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CourseSpringDataRepository.java
+++ springframework.guru/courses/hibernate-jpa-with-springboot/jpa-hibernate/src/main/java/com/in28minute/jpa/hibernate/jpahibernate/repository/CourseSpringDataRepository.java
@@ -3,6 +3,28 @@ package com.in28minute.jpa.hibernate.jpahibernate.repository;
 
 import com.in28minute.jpa.hibernate.jpahibernate.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByName(String name);
+
+    List<Course> findByNameAndId(String name);
+
+    List<Course> countByName(String name);
+
+    List<Course> findByNameOrderByIdDesc(String name);
+
+    List<Course> deleteByName(String name);
+
+    @Query("Select c From Course c where name like '%100 Steps'")
+    List<Course> courseWith100StepsInName();
+
+    @Query(value = "Select * From Course c where name like '%100 Steps'", nativeQuery = true)
+    List<Course> courseWith100StepsInNameUsingNativeQuery();
+
+    @Query(name = "query_get_100_step_courses")
+    List<Course> courseWith100StepsInNameUsingNamedQuery();
 }
