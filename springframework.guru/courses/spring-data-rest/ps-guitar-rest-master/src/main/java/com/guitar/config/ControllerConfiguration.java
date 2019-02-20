ece506e9e20018b30d76121b897718ffd62d63eb
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-20 15:08:24 +0700

    Use ControllerAdvice to handle exception

---
+++ springframework.guru/courses/spring-data-rest/ps-guitar-rest-master/src/main/java/com/guitar/config/ControllerConfiguration.java
@@ -0,0 +1,18 @@
+package com.guitar.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class ControllerConfiguration {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid Data sent to server")
+    public void notValid() {
+
+    }
+}
