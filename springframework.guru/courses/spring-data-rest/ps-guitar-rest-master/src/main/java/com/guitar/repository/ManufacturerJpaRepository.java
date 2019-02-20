f27577cd3c1098f1fe57b7bd6cc24b9ca1404418
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-20 14:41:00 +0700

    Secure methods defined in repository

--- springframework.guru/courses/spring-data-rest/ps-guitar-rest-master/src/main/java/com/guitar/repository/ManufacturerJpaRepository.java
+++ springframework.guru/courses/spring-data-rest/ps-guitar-rest-master/src/main/java/com/guitar/repository/ManufacturerJpaRepository.java
@@ -3,14 +3,19 @@ package com.guitar.repository;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.guitar.model.Manufacturer;
 
 @Repository
 @RepositoryRestResource(path = "companies", collectionResourceRel = "companies")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface ManufacturerJpaRepository extends JpaRepository<Manufacturer, Long> {
 	List<Manufacturer> findByFoundedDateBefore(Date date);
 	
