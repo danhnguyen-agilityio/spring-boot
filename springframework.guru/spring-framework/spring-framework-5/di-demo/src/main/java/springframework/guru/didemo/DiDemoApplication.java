c3b154f987369cb1c810cc0be53187d31cfe767e
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-22 15:26:31 +0700

    Spring framework context

--- springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/DiDemoApplication.java
+++ springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/DiDemoApplication.java
@@ -2,12 +2,18 @@ package springframework.guru.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import springframework.guru.didemo.controlller.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController myController = (MyController) context.getBean("myController");
+
+		myController.hello();
 	}
 
 }
---
+++ springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/controlller/MyController.java
@@ -0,0 +1,11 @@
+package springframework.guru.didemo.controlller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyController {
+
+    public void hello() {
+        System.out.println("Hello world");
+    }
+}
