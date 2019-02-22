88fe71c6be67c37140a6de1c532e46863c2e3a7e
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-22 09:18:56 +0700

    Move repositories folder out of  model folder

--- springframework.guru/spring-framework/spring-framework-5/spring5webapp/src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5webapp/src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp.model.repositories;
+package guru.springframework.spring5webapp.repositories;
 
 import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
