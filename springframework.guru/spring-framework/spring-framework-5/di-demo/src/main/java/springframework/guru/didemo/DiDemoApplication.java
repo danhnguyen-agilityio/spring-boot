4002e98caf5cd0bbe487fb55d8a904b725f97c5a
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-22 15:18:27 +0700

    Initialize dependency inejction demo project

---
+++ springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package springframework.guru.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+
+}
