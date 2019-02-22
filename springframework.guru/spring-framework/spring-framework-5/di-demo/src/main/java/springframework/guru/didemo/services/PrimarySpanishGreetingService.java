8c2b2cccf4bac1b96685e9c84ec444d61d13c31b
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-22 17:17:01 +0700

    Use profile annotation

--- springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/services/PrimarySpanishGreetingService.java
+++ springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/services/PrimarySpanishGreetingService.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello Guru - Primary Service";
+        return "Servicio deSaludo Primario";
     }
 }
