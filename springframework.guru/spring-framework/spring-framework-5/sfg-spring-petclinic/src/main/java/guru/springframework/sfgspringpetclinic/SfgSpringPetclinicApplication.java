bc959e1ac93022a45121eac3e24cc312e8443305
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-25 16:11:21 +0700

    Init project sfg spring petclinic

---
+++ springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/src/main/java/guru/springframework/sfgspringpetclinic/SfgSpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgSpringPetclinicApplication.class, args);
+	}
+
+}
