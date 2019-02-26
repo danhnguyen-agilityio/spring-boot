7bb64d295947f2e68775efd8eefa0ba4443da884
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-26 15:00:05 +0700

    Implement map based service

--- springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/OwenerService.java
+++ springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/OwenerService.java
@@ -2,8 +2,6 @@ package guru.springframework.sfgspringpetclinic.services;
 
 import guru.springframework.sfgspringpetclinic.model.Owner;
 
-import java.util.Set;
-
 public interface OwenerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
