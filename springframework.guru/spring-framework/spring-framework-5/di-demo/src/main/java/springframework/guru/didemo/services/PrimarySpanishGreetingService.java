26ca3237c70fef01d25621601da8aa72f2af46c4
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-02-27 09:23:46 +0700

    Create Spring factory bean

--- springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/services/PrimarySpanishGreetingService.java
+++ springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/services/PrimarySpanishGreetingService.java
@@ -1,12 +1,5 @@
 package springframework.guru.didemo.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
-@Profile("es")
 public class PrimarySpanishGreetingService implements GreetingService {
     private GreetingRepository greetingRepository;
 
