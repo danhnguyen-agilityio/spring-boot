6a27112c026dc215d511353621aad764f833f84d
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-05 16:09:31 +0700

    Config environment properties

--- springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/config/PropertyConfig.java
+++ springframework.guru/spring-framework/spring-framework-5/di-demo/src/main/java/springframework/guru/didemo/config/PropertyConfig.java
@@ -1,14 +1,19 @@
 package springframework.guru.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import springframework.guru.didemo.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -22,7 +27,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(user);
+        fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
