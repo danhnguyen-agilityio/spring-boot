4493fe62cd3b5246ff16b90569d27678c4707325
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-06 15:25:39 +0700

    Config spring boot dev tools, 1. Ctrl shift A , press Registry and click option compiler.automake.allow.when.app.runnung, 2. Setting -> Compiler => click Auto Build option

--- springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Demo spring project demo hello example world");
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
 	}
 }
