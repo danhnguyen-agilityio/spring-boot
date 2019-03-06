67a2cb4bd476e5a5de210f110c09112142d542ea
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-06 17:24:41 +0700

    Define one to one relationship, use Log annotation to persist large object

---
+++ springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,43 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    // A large object for the recipeNotes that will get a clob field inside
+    // to persist the Notes and without having to worry about limitations on the size of that String
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
