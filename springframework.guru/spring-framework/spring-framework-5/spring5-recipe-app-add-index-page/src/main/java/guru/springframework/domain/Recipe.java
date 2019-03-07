3639053492003c3bab2827872c1461acce4c009a
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-07 09:13:16 +0700

    Create enumeration

--- springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Recipe.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Recipe.java
@@ -18,6 +18,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
