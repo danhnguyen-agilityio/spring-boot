85e94d7c507346d308c38aa418c838ee68ab35c4
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-07 16:36:34 +0700

    Create pet type map service

---
+++ springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgspringpetclinic.services;
+
+import guru.springframework.sfgspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
