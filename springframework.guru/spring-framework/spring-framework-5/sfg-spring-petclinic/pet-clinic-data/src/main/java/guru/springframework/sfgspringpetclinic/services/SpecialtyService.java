93c815d0ade9796fd2ba11614cdbf8ff936bdfa6
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-07 17:20:33 +0700

    Create specialty map service

---
+++ springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/pet-clinic-data/src/main/java/guru/springframework/sfgspringpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgspringpetclinic.services;
+
+import guru.springframework.sfgspringpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
