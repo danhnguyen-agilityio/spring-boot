ff56537c266c0a2835162274ddd94191cc606684
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-19 10:05:13 +0700

    2 way to create Mock, that is using MockitoAnnotations.initMocks(this) or @RunWith(MockitoJUnitRunner.class)

--- springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService {
 
     public final RecipeRepository recipeRepository;
--- springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -4,15 +4,20 @@ import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.*;
+
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
 
+//@RunWith(MockitoJUnitRunner.class)
 public class RecipeServiceImplTest {
 
     RecipeServiceImpl recipeService;
