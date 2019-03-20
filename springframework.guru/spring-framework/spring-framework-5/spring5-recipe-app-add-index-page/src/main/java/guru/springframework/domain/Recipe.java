8c7d57fd439b97692d31aa8953a7eb2cfa029cc3
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-20 16:57:54 +0700

    Fix error hashCode

--- springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Recipe.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Recipe.java
@@ -1,13 +1,15 @@
 package guru.springframework.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
-@Data
 public class Recipe {
 
     @Id
