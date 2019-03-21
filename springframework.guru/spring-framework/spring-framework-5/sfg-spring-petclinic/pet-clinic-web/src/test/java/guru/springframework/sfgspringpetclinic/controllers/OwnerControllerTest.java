1cd7b20ec2c8c60edbe4a0eb533ff8c40367dac0
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-21 10:52:06 +0700

    Display owner record

--- springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/pet-clinic-web/src/test/java/guru/springframework/sfgspringpetclinic/controllers/OwnerControllerTest.java
+++ springframework.guru/spring-framework/spring-framework-5/sfg-spring-petclinic/pet-clinic-web/src/test/java/guru/springframework/sfgspringpetclinic/controllers/OwnerControllerTest.java
@@ -14,8 +14,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -73,5 +77,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyZeroInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
