fba143c4c1d2a5b169b2314d5192ac59f7f57b66
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-03-26 14:33:31 +0700

    Docker command

--- springframework.guru/spring-framework/spring-framework-5/spring-boot-mongodb-master/src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ springframework.guru/spring-framework/spring-framework-5/spring-boot-mongodb-master/src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -3,6 +3,32 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// docker logs <container name> => see console output
+// docker logs -f <container name> => see tail console output
+
+// docker run mongo
+// docker run -d mongo
+
+// volume data from host dir to container dir
+// docker run -p 27017:27017 -v /user/database/mongo:/data/db -d mongo => for linux and mac
+
+// docker volume create --name=mongodata
+// docker run -p 27017:27017 -v mongodata:/data/db -d mongo => for windown 10
+
+// docker run -d --hostname guru-rabbit --name some-rabbit -p 8080:15672 -p 5671:5671 -p 5672:5672 rabbitmq:3-management
+
+// docker run --name guru-mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -v sqldb:/var/lib/mysql -p 3306:3306 -d mysql
+
+// build docker image
+// From the directory of the Dockerfile run
+// docker build -t <tag name>
+
+// specify an environment variable for a docker container
+// docker run -e MY_VAR=my_prop <image name>
+
+// shell into a running docker container
+// docker exec -it <container name> bash
+
 @SpringBootApplication
 public class SpringBootMongodbApplication {
 
