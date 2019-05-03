d8bc95b06ebccc08b0525cd280c2d409e4a06e72
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-03 14:09:43 +0700

    Init spring security restful web service

---
+++ pro-spring-boot/spring-security/spring-security-web-service/src/main/java/booking/springsecuritywebservice/SpringSecurityWebServiceApplication.java
@@ -0,0 +1,13 @@
+package booking.springsecuritywebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWebServiceApplication.class, args);
+	}
+
+}
