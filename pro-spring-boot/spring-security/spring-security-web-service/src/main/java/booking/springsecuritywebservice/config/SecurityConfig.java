05f1eb506fdc213568721a060152b5225ddcdb04
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-03 16:40:12 +0700

    Authorization with spring: use url mapping and resource annotations

--- pro-spring-boot/spring-security/spring-security-web-service/src/main/java/booking/springsecuritywebservice/config/SecurityConfig.java
+++ pro-spring-boot/spring-security/spring-security-web-service/src/main/java/booking/springsecuritywebservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package booking.springsecuritywebservice.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,13 +12,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("user").password("user").roles("USER");
+            .withUser("user").password("user").roles("USER")
+            .and()
+            .withUser("admin").password("admin").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // instruct Spring to authenticate all requests using the HTTP Basic authentication scheme
         http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/bookings/**").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and().httpBasic();
 
--- pro-spring-boot/spring-security/spring-security-web-service/src/main/java/booking/springsecuritywebservice/controllers/BookingsResource.java
+++ pro-spring-boot/spring-security/spring-security-web-service/src/main/java/booking/springsecuritywebservice/controllers/BookingsResource.java
@@ -3,6 +3,7 @@ package booking.springsecuritywebservice.controllers;
 import booking.springsecuritywebservice.models.BookingDTO;
 import booking.springsecuritywebservice.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ public class BookingsResource {
     @Autowired
     private BookingService bookingService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value = "/{bookingId}", method = RequestMethod.GET)
     public BookingDTO getBooking(@PathVariable("bookingId") long bookingId) {
         return new BookingDTO(bookingService.getBooking(bookingId));
