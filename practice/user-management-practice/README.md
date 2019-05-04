089a95746405390447cab652e9325a7b1dce085e
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-04 11:38:34 +0700

    Init project

---
+++ practice/user-management-practice/README.md
@@ -0,0 +1,14 @@
+ # SPRING SECURITY PRACTICE: USER MANAGEMENT
+ 
+ ## GOALS
+ 
+ - Build RESTful Web Service
+ - Secures APIs by using token-based authentication
+ 
+ --- 
+ 
+ ## STEPS FINISH PROJECT
+ 
+ 1. [Init project](https://start.spring.io/)
+ 2. []()   
+ 
---
+++ practice/user-management-practice/src/main/java/com/agility/usermanagement/UserManagementPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.agility.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementPracticeApplication.class, args);
+	}
+
+}
