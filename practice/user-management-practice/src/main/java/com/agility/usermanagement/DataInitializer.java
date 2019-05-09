15a2bc43cabf277ddbd0f9bf440aae1330b10fd9
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-09 15:00:18 +0700

    Initialize data if need

---
+++ practice/user-management-practice/src/main/java/com/agility/usermanagement/DataInitializer.java
@@ -0,0 +1,35 @@
+package com.agility.usermanagement;
+
+import com.agility.usermanagement.models.User;
+import com.agility.usermanagement.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * This class used to initial user data right after application context loaded (even in unit test)
+ */
+@Component
+@Slf4j
+public class DataInitializer implements CommandLineRunner {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public DataInitializer(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setUsername("david");
+        user.setPassword("password");
+//        userRepository.save(user);
+    }
+}
