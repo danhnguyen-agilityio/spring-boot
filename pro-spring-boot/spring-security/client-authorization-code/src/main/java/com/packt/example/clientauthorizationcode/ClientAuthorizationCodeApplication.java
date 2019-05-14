2b01556dd09413106f8736f8823363140abbad16
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-14 14:54:25 +0700

    Override the JSESSIONID cookie

--- pro-spring-boot/spring-security/client-authorization-code/src/main/java/com/packt/example/clientauthorizationcode/ClientAuthorizationCodeApplication.java
+++ pro-spring-boot/spring-security/client-authorization-code/src/main/java/com/packt/example/clientauthorizationcode/ClientAuthorizationCodeApplication.java
@@ -2,12 +2,20 @@ package com.packt.example.clientauthorizationcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
-public class ClientAuthorizationCodeApplication {
+public class ClientAuthorizationCodeApplication implements ServletContextInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientAuthorizationCodeApplication.class, args);
 	}
 
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.getSessionCookieConfig().setName("client-session");
+	}
 }
