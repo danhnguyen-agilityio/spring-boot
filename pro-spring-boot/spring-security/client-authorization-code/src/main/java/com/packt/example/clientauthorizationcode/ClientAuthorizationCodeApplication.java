48e318ad77b7f06605abec2466fa06e1aafe03fa
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-14 11:33:28 +0700

    Init client authorization code

---
+++ pro-spring-boot/spring-security/client-authorization-code/src/main/java/com/packt/example/clientauthorizationcode/ClientAuthorizationCodeApplication.java
@@ -0,0 +1,13 @@
+package com.packt.example.clientauthorizationcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAuthorizationCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAuthorizationCodeApplication.class, args);
+	}
+
+}
