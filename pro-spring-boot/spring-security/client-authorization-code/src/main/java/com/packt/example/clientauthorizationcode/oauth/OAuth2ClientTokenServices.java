4afe17bbaeccc1b3d50bea462c45ca5c029d1a8b
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-14 14:32:55 +0700

    Provide CRUD on token

---
+++ pro-spring-boot/spring-security/client-authorization-code/src/main/java/com/packt/example/clientauthorizationcode/oauth/OAuth2ClientTokenServices.java
@@ -0,0 +1,58 @@
+package com.packt.example.clientauthorizationcode.oauth;
+
+import com.packt.example.clientauthorizationcode.security.ClientUserDetails;
+import com.packt.example.clientauthorizationcode.user.ClientUser;
+import com.packt.example.clientauthorizationcode.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.ClientTokenServices;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
+@Service
+public class OAuth2ClientTokenServices implements ClientTokenServices {
+    @Autowired
+    private UserRepository users;
+
+    private ClientUser getClientUser(Authentication authentication) {
+        ClientUserDetails loggedUser = (ClientUserDetails) authentication.getPrincipal();
+        Long userId = loggedUser.getClientUser().getId();
+        return users.findById(userId).orElse(null);
+    }
+
+    @Override
+    public OAuth2AccessToken getAccessToken(OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails, Authentication authentication) {
+        ClientUser clientUser = getClientUser(authentication);
+        String accessToken = clientUser.getAcccessToken();
+        Calendar expirationDate = clientUser.getAccessTokenValidity();
+        if (accessToken == null) return null;
+        DefaultOAuth2AccessToken oAuth2AccessToken = new DefaultOAuth2AccessToken(accessToken);
+        oAuth2AccessToken.setExpiration(expirationDate.getTime());
+        return oAuth2AccessToken;
+    }
+
+    @Override
+    public void saveAccessToken(OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails, Authentication authentication, OAuth2AccessToken accessToken) {
+        Calendar expirationDate = Calendar.getInstance();
+        expirationDate.setTime(accessToken.getExpiration());
+
+        ClientUser clientUser = getClientUser(authentication);
+        clientUser.setAcccessToken(accessToken.getValue());
+        clientUser.setAccessTokenValidity(expirationDate);
+
+        users.save(clientUser);
+    }
+
+    @Override
+    public void removeAccessToken(OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails, Authentication authentication) {
+        ClientUser clientUser = getClientUser(authentication);
+        clientUser.setAcccessToken(null);
+        clientUser.setRefreshToken(null);
+        clientUser.setAccessTokenValidity(null);
+        users.save(clientUser);
+    }
+}
