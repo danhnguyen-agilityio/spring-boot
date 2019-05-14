138d847289581b6b72484e20b7720589e9f3e3d5
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-14 18:00:58 +0700

    Add data initializer

---
+++ pro-spring-boot/spring-security/client-authorization-code/src/main/java/clientauthorizationcode/DataInitializer.java
@@ -0,0 +1,30 @@
+package clientauthorizationcode;
+
+import clientauthorizationcode.user.ClientUser;
+import clientauthorizationcode.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public DataInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.findByUsername("user").orElse(null) == null) {
+            ClientUser clientUser = new ClientUser();
+            clientUser.setUsername("user");
+            clientUser.setPassword(passwordEncoder.encode("user"));
+            userRepository.save(clientUser);
+        }
+    }
+}
