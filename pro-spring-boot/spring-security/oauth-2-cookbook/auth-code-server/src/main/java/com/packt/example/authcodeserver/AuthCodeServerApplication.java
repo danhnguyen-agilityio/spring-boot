a01ebecb1454e8ad1d3d75e5f7fe09fd8ffa56b3
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-15 15:46:16 +0700

    Implement auth code server

---
+++ pro-spring-boot/spring-security/oauth-2-cookbook/auth-code-server/src/main/java/com/packt/example/authcodeserver/AuthCodeServerApplication.java
@@ -0,0 +1,13 @@
+package com.packt.example.authcodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCodeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCodeServerApplication.class, args);
+	}
+
+}
