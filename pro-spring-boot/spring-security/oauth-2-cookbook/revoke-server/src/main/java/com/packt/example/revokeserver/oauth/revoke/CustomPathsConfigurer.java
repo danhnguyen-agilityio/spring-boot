ac9d98fdc408bdc2dd9b5aa484f7175196846b3f
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-16 17:35:33 +0700

    Use password encoder for credential

--- pro-spring-boot/spring-security/oauth-2-cookbook/revoke-server/src/main/java/com/packt/example/revokeserver/oauth/revoke/CustomPathsConfigurer.java
+++ pro-spring-boot/spring-security/oauth-2-cookbook/revoke-server/src/main/java/com/packt/example/revokeserver/oauth/revoke/CustomPathsConfigurer.java
@@ -1,15 +1,24 @@
 package com.packt.example.revokeserver.oauth.revoke;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.ClientDetailsUserDetailsService;
 
 @Configuration
 public class CustomPathsConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // the size can be between 4 to 31
+        return new BCryptPasswordEncoder(4);
+    }
 
     @Autowired
     private ClientDetailsService clientDetailsService;
@@ -18,7 +27,7 @@ public class CustomPathsConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         ClientDetailsUserDetailsService userDetailsService
             = new ClientDetailsUserDetailsService(clientDetailsService);
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
