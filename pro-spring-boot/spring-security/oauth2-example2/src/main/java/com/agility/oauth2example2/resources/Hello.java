18b3ca622fbab0aff4b8999294efdf505e7151ff
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-21 14:37:08 +0700

    Injecting authenticated user

--- pro-spring-boot/spring-security/oauth2-example2/src/main/java/com/agility/oauth2example2/resources/Hello.java
+++ pro-spring-boot/spring-security/oauth2-example2/src/main/java/com/agility/oauth2example2/resources/Hello.java
@@ -6,6 +6,8 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping(
     value = {"/api/hello"},
@@ -16,7 +18,7 @@ public class Hello {
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Welcome greetings(@RequestParam("name") String name) {
-        return new Welcome(name);
+    public Welcome greetings(@RequestParam("name") String name, Principal principal) {
+        return new Welcome(name + " (" + principal.getName() + ")");
     }
 }
