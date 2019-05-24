593efa7a5c443c732df9127da490ead68d178fee
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-24 15:53:47 +0700

    Remove token when delete user

--- spring-security/practice/oauth2/user-management/authorization-server/src/main/java/com/agility/authorizationserver/controllers/UserController.java
+++ spring-security/practice/oauth2/user-management/authorization-server/src/main/java/com/agility/authorizationserver/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.agility.authorizationserver.controllers;
 import com.agility.authorizationserver.exceptions.ResourceNotFoundException;
 import com.agility.authorizationserver.models.User;
 import com.agility.authorizationserver.repository.UserRepository;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ import static com.agility.authorizationserver.exceptions.CustomError.USER_NOT_FO
 public class UserController {
 
     private UserRepository userRepository;
+    private DefaultTokenServices tokenServices;
 
-    public UserController(UserRepository userRepository) {
+
+    public UserController(UserRepository userRepository, DefaultTokenServices tokenServices) {
         this.userRepository = userRepository;
+        this.tokenServices = tokenServices;
     }
 
     @DeleteMapping("/{id}")
@@ -27,6 +31,9 @@ public class UserController {
         if (user == null) {
             throw new ResourceNotFoundException(USER_NOT_FOUND);
         }
+
+        // TODO:: Revoke token
+
         userRepository.delete(user);
     }
 }
