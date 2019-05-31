a8407f1019b1e70cbf3ab73d7f4e7f93822790e4
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-05-31 09:38:53 +0700

    Fix error 401 Unauthorize when POST, PUT, DELETE request in postman

--- spring-security/practice/okta/usermanagement/src/main/java/com/agility/usermanagement/configs/SecurityConfig.java
+++ spring-security/practice/okta/usermanagement/src/main/java/com/agility/usermanagement/configs/SecurityConfig.java
@@ -19,21 +19,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/api/v1/public/signup", "/");
+        web.ignoring().antMatchers("/api/v1/public/signup");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors()
-            .and().authorizeRequests()
+        http.cors().and().csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/v1/public/**",  "/").permitAll()
             .anyRequest().authenticated()
             .and()
             .oauth2ResourceServer().jwt();
     }
 
     /**
-     * Enable Cors
+     * Enable CORS
      */
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
