c92b40060048789cd5586de8e5af7f1203152f3e
Danh Nguyen <danh.nguyen@asnet.com.vn>
2019-06-18 14:40:12 +0700

    Refactor code

--- spring-security/practice/okta/usermanagement/src/main/java/com/agility/usermanagement/configs/SecurityConfig.java
+++ spring-security/practice/okta/usermanagement/src/main/java/com/agility/usermanagement/configs/SecurityConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,11 +14,6 @@ import java.util.Arrays;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/api/v1/public/signup");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
