030149aa6854d5ca9098e94da8b1e27e2a301364
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-08-06 17:33:25 +0700

    Thread basic

---
+++ springframework.guru/courses/multi-threading/Thread1/src/com/agility/demo1/App.java
@@ -0,0 +1,28 @@
+package com.agility.demo1;
+
+class Runner extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) {
+        Runner runner1 = new Runner();
+        Runner runner2 = new Runner();
+
+        runner1.start();
+        runner2.start();
+    }
+}
---
+++ springframework.guru/courses/multi-threading/Thread1/src/com/agility/demo2/App.java
@@ -0,0 +1,28 @@
+package com.agility.demo2;
+
+class Runner implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runner());
+        Thread thread2 = new Thread(new Runner());
+
+        thread1.start();
+        thread2.start();
+    }
+}
+
