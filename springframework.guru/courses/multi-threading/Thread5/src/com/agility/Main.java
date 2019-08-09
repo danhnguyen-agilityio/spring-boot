68047e6a496a19ae2f0f681fe805ac97ca12f983
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-08-09 17:41:24 +0700

    Executor service demo

---
+++ springframework.guru/courses/multi-threading/Thread5/src/com/agility/Main.java
@@ -0,0 +1,53 @@
+package com.agility;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Processor implements Runnable {
+    private int id;
+
+    public Processor(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Starting: " + id);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Completed: " + id);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // First, 2 threads will run 2 first task, ( total 5 tasks)
+        /// After one thread finish  process, thread will continue another task
+        // Wait not have any task to implement
+        // => executor service terminate
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++) {
+            executorService.submit(new Processor(i));
+        }
+
+        executorService.shutdown();
+
+        System.out.println("All tasks submitted");
+
+        try {
+            executorService.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All tasks completed");
+    }
+}
