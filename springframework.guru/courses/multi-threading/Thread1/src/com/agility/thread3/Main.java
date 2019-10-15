8ee903d6a617c22ce7e0fd120c036b3f16e513f7
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-10-15 09:13:06 +0700

    count down latch

--- springframework.guru/courses/multi-threading/Thread1/src/com/agility/thread3/Main.java
+++ springframework.guru/courses/multi-threading/Thread1/src/com/agility/thread3/Main.java
@@ -9,7 +9,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        com.agility.Main main = new com.agility.Main();
+        Main main = new Main();
         main.doWork();
     }
 
