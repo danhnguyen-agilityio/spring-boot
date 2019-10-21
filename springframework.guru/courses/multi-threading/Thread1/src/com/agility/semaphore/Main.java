a40cc1a4b22f0aebcfb61d79f05aaf155157a6b2
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-10-21 15:57:34 +0700

    Semaphore, keeps a count of the number available

---
+++ springframework.guru/courses/multi-threading/Thread1/src/com/agility/semaphore/Main.java
@@ -0,0 +1,26 @@
+package com.agility.semaphore;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 200; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    Connection.getInstance().connect();
+                }
+            });
+        }
+
+        executor.shutdown();
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
