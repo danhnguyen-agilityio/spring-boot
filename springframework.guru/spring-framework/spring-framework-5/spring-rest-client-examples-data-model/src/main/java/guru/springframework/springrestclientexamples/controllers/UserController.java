1caf4ecf029acee4e2d917886263b40baf84f04f
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-10-23 14:42:04 +0700

    Reactive with Spring WebClient

--- springframework.guru/spring-framework/spring-framework-5/spring-rest-client-examples-data-model/src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
+++ springframework.guru/spring-framework/spring-framework-5/spring-rest-client-examples-data-model/src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
@@ -27,15 +27,22 @@ public class UserController {
     @PostMapping("/users")
     public String formPost(Model model, ServerWebExchange serverWebExchange) {
 
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+//        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+//
+//        Integer limit = new Integer(map.get("limit").get(0));
+//
+//        if (limit == null || limit == 0) {
+//            limit = 10;
+//        }
+//
+//        model.addAttribute("users", apiService.getUsers(limit));
+//
+//        return "userlist";
 
-        Integer limit = new Integer(map.get("limit").get(0));
-
-        if (limit == null || limit == 0) {
-            limit = 10;
-        }
-
-        model.addAttribute("users", apiService.getUsers(limit));
+        model.addAttribute("users",
+                apiService.getUsers(serverWebExchange
+                    .getFormData()
+                    .map(data -> new Integer(data.getFirst("limit")))));
 
         return "userlist";
     }
--- springframework.guru/spring-framework/spring-framework-5/spring-rest-client-examples-data-model/src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ springframework.guru/spring-framework/spring-framework-5/spring-rest-client-examples-data-model/src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -3,9 +3,13 @@ package guru.springframework.springrestclientexamples.services;
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -29,6 +33,18 @@ public class ApiServiceImpl implements ApiService {
 
         UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
         return userData.getData();
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(res -> res.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
     }
 
 }
