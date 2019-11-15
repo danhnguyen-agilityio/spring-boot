749bd0bb27bd87cad9e00b7d7b184cf1613e38ff
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-11-15 10:14:17 +0700

    Not need above config for swagger UI

--- springframework.guru/spring-framework/spring-framework-5/spring5-mvc-rest-master/src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ springframework.guru/spring-framework/spring-framework-5/spring5-mvc-rest-master/src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.w3c.dom.DocumentType;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig { //extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
@@ -25,12 +24,12 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .pathMapping("/");
     }
 
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
