63c9440e4ea5c92b94c05f991e13ce50b97c2ffd
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-11-19 15:53:27 +0700

    init structure spring mvc rest

---
+++ springframework.guru/spring-framework/spring-framework-5/spring-mvc-rest/src/main/java/guru/springframework/springmvcrest/domain/Customer.java
@@ -0,0 +1,4 @@
+package guru.springframework.springmvcrest.domain;
+
+public class Customer {
+}
---
+++ springframework.guru/spring-framework/spring-framework-5/spring-mvc-rest/src/test/java/guru/springframework/springmvcrest/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
