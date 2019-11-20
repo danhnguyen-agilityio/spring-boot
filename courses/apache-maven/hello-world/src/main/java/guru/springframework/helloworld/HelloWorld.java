01d673e1bd01f03ef8d839e80a0166494ac88026
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-11-20 17:01:58 +0700

    Add dependency to project

--- courses/apache-maven/hello-world/src/main/java/guru/springframework/helloworld/HelloWorld.java
+++ courses/apache-maven/hello-world/src/main/java/guru/springframework/helloworld/HelloWorld.java
@@ -1,7 +1,11 @@
 package guru.springframework.helloworld;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
