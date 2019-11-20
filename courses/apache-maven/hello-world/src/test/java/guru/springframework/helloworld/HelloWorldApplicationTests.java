d35f97a5ae4c6d0fcdfbdf5b5cf0af93c936cdbf
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-11-20 16:51:15 +0700

    Create new maven project with intellj

---
+++ courses/apache-maven/hello-world/src/test/java/guru/springframework/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
