4efea29fb044404308e1654c454af78f504acbe9
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-11-22 10:26:53 +0700

    Use maven plugin from maven env

---
+++ courses/apache-maven/hello-world/src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldTest {
+
+    public void testHello() {
+        System.out.println("Hello");
+    }
+
+    public void testWorld() {
+        System.out.println("World");
+    }
+}
