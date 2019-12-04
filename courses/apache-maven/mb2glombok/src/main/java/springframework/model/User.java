8577f039c8dc2471a7af6636a4799eaead7f9057
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-04 14:23:05 +0700

    Generating java classes with project lombok and maven

---
+++ courses/apache-maven/mb2glombok/src/main/java/springframework/model/User.java
@@ -0,0 +1,16 @@
+package springframework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
