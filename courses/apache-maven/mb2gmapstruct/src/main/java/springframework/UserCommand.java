f07f5191392b1019c58618c63ba3ae6ff8b213b0
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-04 15:10:41 +0700

    MapStruct and Maven

---
+++ courses/apache-maven/mb2gmapstruct/src/main/java/springframework/UserCommand.java
@@ -0,0 +1,22 @@
+package springframework;
+
+public class UserCommand {
+    String firstName;
+    String username;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ courses/apache-maven/mb2gmapstruct/src/main/java/springframework/UserMapper.java
@@ -0,0 +1,14 @@
+package springframework;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
+}
