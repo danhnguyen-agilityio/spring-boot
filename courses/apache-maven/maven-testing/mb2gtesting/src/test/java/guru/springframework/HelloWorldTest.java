38e83c414176fa9624987f2d3a1fee35b1d1c7e7
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-04 16:20:10 +0700

    Maven surefire for junit 4

--- courses/apache-maven/maven-testing/mb2gtesting/src/test/java/guru/springframework/HelloWorldTest.java
+++ courses/apache-maven/maven-testing/mb2gtesting/src/test/java/guru/springframework/HelloWorldTest.java
@@ -1,14 +1,14 @@
 package guru.springframework;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class HelloWorldTest {
 
-    // Surefire plugin is gonna pick up anything that begins or ends with test
-    // This is the default behaviour of Maven
-    // It gonna be looking for things methods that begin with test
-    // Example: HelloWorldTest,testHello
-    public void testHello() {
+    @Test
+    public void hello() {
         HelloWorld helloWorld = new HelloWorld();
-        assert("Hello world".equals(helloWorld.hello()));
+        assertEquals("Hello world", helloWorld.hello());
     }
 }
