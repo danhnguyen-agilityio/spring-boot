b46691b0da70e5e2c4540cd05f27c566300b86b3
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-05 09:55:48 +0700

    Failsafe plugin executes test methods in classes whose name starts withIT

---
+++ courses/apache-maven/maven-testing/mb2gtesting/src/test/java/guru/springframework/HelloWorldIT.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+public class HelloWorldIT {
+
+    @Test
+    public void myPauxIntegeration() {
+        System.out.println("My IT Ran");
+    }
+}
