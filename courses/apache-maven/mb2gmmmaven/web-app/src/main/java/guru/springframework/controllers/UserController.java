22e01f85c7a9275a737338c4c3dded0ee2c8dde0
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-06 17:50:45 +0700

    Maven dependency between modules

---
+++ courses/apache-maven/mb2gmmmaven/web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -0,0 +1,11 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.UserCommand;
+import guru.springframework.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+        return new User();
+    }
+}
