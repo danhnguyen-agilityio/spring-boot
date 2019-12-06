88187849bca80cc2364a798d88aac3dcc2fe66e3
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-06 09:15:59 +0700

    Create maven model from intellij

---
+++ courses/apache-maven/mb2gmmmaven/from-intellij/src/main/java/guru/springframework/intellij/HelloFromJetbrains.java
@@ -0,0 +1,8 @@
+package guru.springframework.intellij;
+
+public class HelloFromJetbrains {
+
+    public String hello() {
+        return "Hello Jetbrains";
+    }
+}
---
+++ courses/apache-maven/mb2gmmmaven/jpa-entities/src/main/java/guru/springframework/entities/User.java
@@ -0,0 +1,21 @@
+package guru.springframework.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
