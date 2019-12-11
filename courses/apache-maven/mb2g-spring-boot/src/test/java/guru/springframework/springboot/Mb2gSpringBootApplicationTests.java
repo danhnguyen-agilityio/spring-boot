d44b4a7ac5d9b0f4b705784ba9d63ec88c073b95
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-11 11:03:54 +0700

    Create project with the spring initialize

---
+++ courses/apache-maven/mb2g-spring-boot/src/test/java/guru/springframework/springboot/Mb2gSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mb2gSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
