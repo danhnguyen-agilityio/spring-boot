47e58d6ef26b93e9a436d4f378a36725a18b06bd
Nguyen Danh <danh.nguyen@asnet.com.vn>
2019-12-18 09:48:40 +0700

    Create snapshot repository nexus

---
+++ courses/apache-maven/mb2g-testing-master/src/main/java/guru/springframework/JavaHelloWorld.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import com.microsoft.services.sharepoint.SharePointClient;
+
+/**
+ * Created by jt on 2018-12-09.
+ */
+public class JavaHelloWorld {
+    public String getHello(){
+        return "Hello World";
+    }
+
+}
